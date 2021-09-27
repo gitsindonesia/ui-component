a7b64206a5193dacb98af5490ef0f50d666a36a9
Warsono <warsono16694@gmail.com>
2021-09-27 14:50:54 +0700

    feat(v-breadcrumbs): improved styles and active state

--- src/components/VBreadcrumbs/VBreadcrumbs.vue
+++ src/components/VBreadcrumbs/VBreadcrumbs.vue
@@ -15,20 +15,22 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="mb-3">
+  <div class="mb-3 flex gap-2 items-center">
     <template v-for="(item, index) in props.items" :key="index">
       <router-link
         :to="item.to"
-        class="text-sm text-gray-800 font-medium hover:text-primary-600"
+        exact-active-class="text-primary-500"
+        class="font-medium hover:text-primary-600"
       >
         {{ item.title }}
       </router-link>
-      <span
-        v-if="index + 1 < props.items.length"
-        class="text-sm text-gray-400 font-medium mx-2"
-      >
-        {{ props.divider }}
-      </span>
+      <template v-if="index + 1 < props.items.length">
+        <slot name="divider">
+          <span class="text-sm text-gray-400 font-medium mx-2">
+            {{ props.divider }}
+          </span>
+        </slot>
+      </template>
     </template>
   </div>
 </template>
