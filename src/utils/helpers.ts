dceac0e747f2649f88d6c6fdd5de18ba0e707d03
Warsono <warsono16694@gmail.com>
2021-09-28 17:48:52 +0700

    feat(helpers): added some new helper functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,7 @@
+import dayjs from 'dayjs';
+
+export const TZ = 'Asia/Jakarta';
+
 export const get = (obj: any, path: any, defaultValue = undefined) => {
   const travel = (regexp: any) =>
     String.prototype.split
@@ -10,3 +14,32 @@ export const get = (obj: any, path: any, defaultValue = undefined) => {
   const result = travel(/[,[\]]+?/) || travel(/[,[\].]+?/);
   return result === undefined || result === obj ? defaultValue : result;
 };
+
+export const stripTags = (str: string) => str?.replace(/(<([^>]+)>)/gi, '');
+
+export const localDate = (str: string, locale = 'id') => {
+  return dayjs.tz(str, TZ).format('DD/MM/YYYY HH:mm:ss');
+};
+
+export const normalizeDate = (str: string) => {
+  const date = str?.replace('T', ' ').replace('.000+00:00', '');
+  return dayjs(date).tz(TZ).toDate();
+};
+
+export const toFormData = <T extends Record<string, any>>(values: T) => {
+  const data = new FormData();
+  for (let key in values) {
+    data.append(key, values[key]);
+  }
+  return data;
+};
+
+export const toOptions = <T extends Record<string, any>[]>(
+  items: T,
+  text = 'text',
+  value = 'value',
+) =>
+  items?.map((item) => ({
+    text: item[text],
+    value: item[value],
+  })) || [];
