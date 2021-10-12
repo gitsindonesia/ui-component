d7db2ec3db3f8ac896732798d85a0805eb69d996
Warsono <warsono16694@gmail.com>
2021-10-12 17:04:51 +0700

    fix: shims vue

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,11 +3,9 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
-// declare module '@vue/runtime-core' {
-//   import type { AbstractContainer } from './container';
-//   export interface ComponentCustomProperties {
-//     $container: typeof AbstractContainer
-//   }
-// }
-
-export {};
+// // declare module '@vue/runtime-core' {
+// //   import type { AbstractContainer } from './container';
+// //   export interface ComponentCustomProperties {
+// //     $container: typeof AbstractContainer
+// //   }
+// // }
