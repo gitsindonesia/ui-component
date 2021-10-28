6edc5dcc7a59b0895aa2d5f90025ba384066dd0b
Warsono <warsono16694@gmail.com>
2021-10-28 14:31:11 +0700

    feat(v-file-input): reworked and fixed bug file input no cleared on remove

--- src/components/VFileUpload/VFileUpload.vue
+++ src/components/VFileUpload/VFileUpload.vue
@@ -5,13 +5,24 @@ export default {
 </script>
 
 <script setup lang="ts">
-import {ref, toRefs, computed, watch} from 'vue';
-import {CameraIcon, PlusIcon} from '@heroicons/vue/solid';
+import {ref, toRefs, computed, watch, PropType} from 'vue';
+import {CameraIcon, PlusIcon, TrashIcon} from '@heroicons/vue/solid';
 import VBtn from '../VBtn/VBtn.vue';
 import VInput from '../VInput/VInput.vue';
 import {ErrorMessage} from 'vee-validate';
+import {AnyMessageParams} from 'yup/lib/types';
+
+type FileValue = File | FileList | File[] | Record<string, any> | null;
 
 const props = defineProps({
+  value: {
+    type: Object as PropType<FileValue>,
+    default: null,
+  },
+  modelValue: {
+    type: Object as PropType<FileValue>,
+    default: null,
+  },
   button: {
     type: Boolean,
     default: false,
@@ -27,14 +38,6 @@ const props = defineProps({
   image: {
     type: Boolean,
     default: false,
-  },
-  value: {
-    type: Object,
-    default: null,
-  },
-  modelValue: {
-    type: Object,
-    default: null,
   },
   accept: {
     type: String,
@@ -59,6 +62,10 @@ const props = defineProps({
   placeholder: {
     type: String,
     default: 'Browse file...',
+  },
+  hint: {
+    type: String,
+    default: '',
   },
   id: {
     type: String,
@@ -121,24 +128,13 @@ const emit = defineEmits([
 ]);
 
 const {
-  button,
-  rounded,
   image,
   value,
   modelValue,
   accept,
   inputProps,
-  errorMessages,
-  name,
-  placeholder,
-  id,
   readonly,
   disabled,
-  browseText,
-  changeText,
-  removeText,
-  viewFileText,
-  theme,
   multiple,
 } = toRefs(props);
 
@@ -146,7 +142,7 @@ const sizeClass = computed(() =>
   props.full ? 'w-full' : 'w-full sm:w-[180px] h-[180px]',
 );
 
-const file = ref(null);
+const innerValue = ref<FileValue>(null);
 const fileRef = ref<HTMLInputElement | null>(null);
 const previewURL = ref<string | null>(null);
 const hasInitialValue = ref(false);
@@ -165,55 +161,72 @@ const pickFile = () => {
 const onFileChanged = (event: any) => {
   previewURL.value = null;
 
-  const fileTarget = event.target?.files[0];
+  const files = event.target?.files;
 
-  if (fileTarget) {
-    file.value = fileTarget;
+  if (multiple.value) {
+    innerValue.value = files;
 
-    if (image.value && file.value) {
-      previewURL.value = URL.createObjectURL(file.value);
+    emit('change', files);
+    emit('update:modelValue', files);
+    emit('update:value', files);
+  } else {
+    const firstFile = files[0];
+
+    innerValue.value = firstFile;
+
+    if (image.value && firstFile) {
+      previewURL.value = URL.createObjectURL(firstFile.value);
     }
 
-    emit('input', fileTarget);
-    emit('change', fileTarget);
-    emit('update:modelValue', fileTarget);
-    emit('update:value', fileTarget);
-  } else {
-    removeFile();
+    emit('change', firstFile);
+    emit('update:modelValue', firstFile);
+    emit('update:value', firstFile);
   }
 };
 
 const removeFile = () => {
-  file.value = null;
+  innerValue.value = null;
   previewURL.value = null;
   hasInitialValue.value = false;
 
+  emit('change', null);
+  emit('update:modelValue', null);
+  emit('update:value', null);
   emit('removed');
 };
 
 const setInitialValue = (val: any) => {
+  const isFile = val instanceof File;
+  const isFileList = val instanceof FileList;
+
+  if (isFile || isFileList) {
+    innerValue.value = val;
+  }
+
   if (image.value && val) {
     hasInitialValue.value = true;
-
-    previewURL.value = URL.createObjectURL(file.value);
-  } else {
-    removeFile();
+    previewURL.value = URL.createObjectURL(innerValue.value);
   }
 };
 
-const hasFile = computed(() => !!file.value || !!hasInitialValue.value);
+const hasFile = computed(() => {
+  return !!innerValue.value || !!hasInitialValue.value;
+});
 
 const fileName = computed(
-  () => (file.value || value.value || modelValue.value || {name: ''}).name,
+  () =>
+    (innerValue.value || value.value || modelValue.value || {name: ''}).name,
 );
 
 const fileURL = computed(
-  () => (file.value || value.value || modelValue.value || {file: ''}).file,
+  () =>
+    (innerValue.value || value.value || modelValue.value || {file: ''}).file,
 );
 
 watch(
   value,
   (val) => {
+    console.log('value', val);
     setInitialValue(val);
   },
   {immediate: true},
@@ -222,24 +235,32 @@ watch(
 watch(
   modelValue,
   (val) => {
+    console.log('modelValue', val);
     setInitialValue(val);
   },
   {immediate: true},
 );
 
 const disabledClass = computed(() => {
-  return disabled.value || readonly.value
-    ? 'bg-[#F5F5F5] text-[#BDBDBD] placeholder-[#BDBDBD] cursor-not-allowed border-secondary-300'
-    : '';
+  return disabled.value || readonly.value ? 'disabled-input' : '';
 });
 </script>
 
 <template>
-  <template v-if="theme === 'button'">
+  <div v-if="theme === 'button'" class="flex gap-2">
     <div v-if="hasFile" class="flex gap-2 items-center">
       <div
-        class="border rounded-10 px-4 py-2 truncate text-center"
+        class="
+          px-4
+          py-2
+          truncate
+          text-center
+          border
+          rounded
+          hover:border-gray-400
+        "
         :class="[disabledClass]"
+        @click="pickFile"
       >
         {{ fileName }}
       </div>
@@ -254,26 +275,51 @@ const disabledClass = computed(() => {
       >
         {{ viewFileText }}
       </v-btn>
+
+      <div
+        v-if="!hasFile && !hidePlaceholder"
+        class="text-sm mt-1 text-gray-500"
+      >
+        {{ placeholder }}
+      </div>
     </div>
-    <VBtn
-      v-else
-      uppercase
-      outlined
-      type="button"
-      color="primary"
-      class="line-clamp"
-      :disabled="readonly || disabled"
-      @click="pickFile"
-    >
-      <slot name="icon.plus">
-        <PlusIcon class="w-5 h-5 mr-2" />
-      </slot>
-      {{ browseText }}
-    </VBtn>
-    <div v-if="!hasFile && !hidePlaceholder" class="text-sm mt-1 text-gray-500">
-      {{ placeholder }}
+    <div class="flex gap-2 items-center">
+      <VBtn
+        uppercase
+        outlined
+        type="button"
+        color="primary"
+        class="line-clamp"
+        :disabled="readonly || disabled"
+        @click="pickFile"
+      >
+        <slot v-if="!hasFile" name="icon.plus">
+          <PlusIcon class="w-5 h-5 mr-2" />
+        </slot>
+        <slot v-if="hasFile" name="icon.plus">
+          <PlusIcon class="w-5 h-5 mr-2" />
+        </slot>
+        {{ hasFile ? changeText : browseText }}
+      </VBtn>
+
+      <VBtn
+        v-if="hasFile"
+        small
+        dense
+        outlined
+        type="button"
+        color="error"
+        class="space-x-2"
+        :disabled="readonly || disabled"
+        @click="removeFile"
+      >
+        <slot name="icon.trash">
+          <TrashIcon class="w-5 h-5" />
+        </slot>
+        <span>{{ removeText }}</span>
+      </VBtn>
     </div>
-  </template>
+  </div>
 
   <div v-else-if="theme === 'image'">
     <div
@@ -310,6 +356,7 @@ const disabledClass = computed(() => {
       </template>
     </div>
   </div>
+
   <div v-else>
     <v-input
       :model-value="fileName"
@@ -390,15 +437,28 @@ const disabledClass = computed(() => {
     </div>
   </div>
 
-  <div v-if="placeholder && !hidePlaceholder" class="text-xs mt-2 text-black">
-    {{ placeholder }}
-  </div>
+  <slot name="hint">
+    <div v-if="hint" class="text-xs mt-2 text-black">
+      {{ hint }}
+    </div>
+  </slot>
 
-  <ErrorMessage
-    v-if="errorMessages.length"
-    class="text-error-500 text-sm"
+  <slot
+    name="error"
+    :error="error"
+    :error-messages="errorMessages"
     :name="name"
-  />
+  >
+    <ErrorMessage
+      v-if="errorMessages.length"
+      class="text-error-500 text-sm"
+      :name="name"
+    />
+  </slot>
 </template>
 
-<style scoped></style>
+<style scoped>
+.disabled-input {
+  @apply bg-[#F5F5F5] text-[#BDBDBD] placeholder-[#BDBDBD] cursor-not-allowed border-secondary-300;
+}
+</style>
