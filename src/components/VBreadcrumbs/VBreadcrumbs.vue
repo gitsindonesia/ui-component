4250a9eee36d4b82dff96213c25af736e1aa9cc4
Warsono <warsono16694@gmail.com>
2021-10-28 16:59:40 +0700

    feat(v-breadcrumbs): added custom slots: `title.index` and `item.index`

--- src/components/VBreadcrumbs/VBreadcrumbs.vue
+++ src/components/VBreadcrumbs/VBreadcrumbs.vue
@@ -17,20 +17,24 @@ const props = defineProps({
 <template>
   <div class="mb-3 flex gap-2 items-center">
     <template v-for="(item, index) in props.items" :key="index">
-      <router-link
-        :to="item.to"
-        exact-active-class="text-primary-500"
-        class="font-medium hover:text-primary-600"
-      >
-        {{ item.title }}
-      </router-link>
-      <template v-if="index + 1 < props.items.length">
-        <slot name="divider">
-          <span class="text-sm text-gray-400 font-medium mx-2">
-            {{ props.divider }}
-          </span>
-        </slot>
-      </template>
+      <slot :name="`item.${index}`">
+        <router-link
+          :to="item.to"
+          exact-active-class="text-primary-500"
+          class="font-medium hover:text-primary-600"
+        >
+          <slot :name="`title.${index}`">
+            {{ item.title }}
+          </slot>
+        </router-link>
+        <template v-if="index + 1 < props.items.length">
+          <slot name="divider">
+            <span class="text-sm text-gray-400 font-medium mx-2">
+              {{ props.divider }}
+            </span>
+          </slot>
+        </template>
+      </slot>
     </template>
   </div>
 </template>
