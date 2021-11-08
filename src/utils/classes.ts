20b98648a4e866bb5021b93b484e9c6255f5a01c
Warsono <warsono16694@gmail.com>
2021-11-08 14:18:23 +0700

    fix: default improved input classes

--- src/utils/classes.ts
+++ src/utils/classes.ts
@@ -4,16 +4,20 @@ export const inputDisabledClasses =
 export const inputClasses =
   'border shadow-sm rounded-md text-gray-800 ' + inputDisabledClasses;
 
-export const inputErrorClasses = 'border-error-500 placeholder-error-500 text-error-500 focus:border-error-500 focus:ring-error-500'
+export const inputErrorClasses =
+  'border-error-500 placeholder-error-500 text-error-500 focus-visible:ring-error-500 focus-visible:border-error-500 ring-error-500 focus:border-error-500 focus:ring-error-500';
 
 export const useInputClasses = (error = false) => {
-  let className = ['transition duration-300 border shadow-sm rounded-md text-gray-800 focus:border-primary-500 focus:ring-primary-500', inputDisabledClasses];
+  let className = [
+    'transition duration-300 border shadow-sm rounded-md text-gray-800 focus:border-primary-500 focus:ring-primary-500',
+    inputDisabledClasses,
+  ];
 
   if (error) {
     className.push(inputErrorClasses);
   } else {
-    className.push('border-gray-200');
+    className.push('border-gray-300');
   }
 
-  return className.join(' ')
+  return className.join(' ');
 };
