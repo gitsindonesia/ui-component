95b4acb2fda77b22c03d21e38c64a88bfc3b6c5e
Warsono <warsono16694@gmail.com>
2022-01-07 16:51:27 +0700

    chore: optimize some deps

--- vite.config.js
+++ vite.config.js
@@ -33,4 +33,16 @@ export default defineConfig({
       ],
     },
   },
+  optimizeDeps: {
+    include: [
+      'vue-remix-icons',
+      '@heroicons/vue',
+      'vee-validate',
+      '@headlessui/vue',
+      'pagino',
+      '@popperjs/core',
+      'vue-feather',
+      'vue-router',
+    ],
+  },
 });
