26c3c799293a3aed872f0ce31e5fa34a2825e2ae
Warsono <warsono16694@gmail.com>
2022-03-08 21:28:39 +0700

    chore: fix storybook setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,8 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../packages/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
@@ -10,5 +11,9 @@ module.exports = {
   "framework": "@storybook/vue3",
   "core": {
     "builder": "storybook-builder-vite"
-  }
+  },
+  async viteFinal(config, {configType}) {
+    config.resolve.dedupe = ["@storybook/client-api"]
+    return config
+},
 }
