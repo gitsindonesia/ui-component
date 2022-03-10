6dccc64195a9c6840a89cda1c17dfc39aca6ef56
Warsono <warsono16694@gmail.com>
2022-03-10 19:40:53 +0700

    playground: update homepage

--- apps/playground/src/views/HomeView.vue
+++ apps/playground/src/views/HomeView.vue
@@ -1,11 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-  <v-card> lorem </v-card>
-
-  <v-input />
-
-  <v-btn>Button</v-btn>
+  <h1 class="text-3xl font-bold">GITS UI Component Playground</h1>
 </template>
