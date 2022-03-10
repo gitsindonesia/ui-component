02cc84231b2bfd043bbf65754c9ce6fb1f603f34
Warsono <warsono16694@gmail.com>
2022-03-10 19:39:29 +0700

    docs: breadcrumbs playground

--- apps/playground/src/views/components/breadcrumbs.vue
+++ apps/playground/src/views/components/breadcrumbs.vue
@@ -1,6 +1,21 @@
-b
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const items = [
+  {
+    title: 'Home',
+    to: '/',
+  },
+  {
+    title: 'Link',
+    to: '/',
+  },
+  {
+    title: 'Link',
+    to: '/',
+  },
+];
+</script>
 
 <template>
-  <v-breadcrumbs> lorem </v-breadcrumbs>
+  <v-breadcrumbs :items="items"> lorem </v-breadcrumbs>
+  <v-breadcrumbs :items="items" divider="-"> lorem </v-breadcrumbs>
 </template>
