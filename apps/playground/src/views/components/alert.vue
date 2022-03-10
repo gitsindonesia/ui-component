df0cdcd2562b50c8376dd3083a3f2fc32f65b8fb
Warsono <warsono16694@gmail.com>
2022-03-10 19:35:59 +0700

    docs: add alert playground

--- apps/playground/src/views/components/alert.vue
+++ apps/playground/src/views/components/alert.vue
@@ -1,11 +1,41 @@
 <script setup lang="ts"></script>
 
 <template>
-  <v-alert> lorem ipsum dolor sit amet </v-alert>
-  <v-alert color="primary"> lorem ipsum dolor sit amet </v-alert>
-  <v-alert color="secondary"> lorem ipsum dolor sit amet </v-alert>
-  <v-alert color="info"> lorem ipsum dolor sit amet </v-alert>
-  <v-alert color="warning"> lorem ipsum dolor sit amet </v-alert>
-  <v-alert color="error"> lorem ipsum dolor sit amet </v-alert>
-  <v-alert color="dark"> lorem ipsum dolor sit amet </v-alert>
+  <h1 class="font-bold text-xl mb-5">Alert</h1>
+  <div class="space-y-2">
+    <v-alert> lorem ipsum dolor sit amet </v-alert>
+    <v-alert color="primary"> lorem ipsum dolor sit amet </v-alert>
+    <v-alert color="secondary"> lorem ipsum dolor sit amet </v-alert>
+    <v-alert color="info"> lorem ipsum dolor sit amet </v-alert>
+    <v-alert color="warning"> lorem ipsum dolor sit amet </v-alert>
+    <v-alert color="error"> lorem ipsum dolor sit amet </v-alert>
+  </div>
+
+  <h2 class="font-bold text-lg mt-8 mb-2">Icon</h2>
+  <div class="space-y-2">
+    <v-alert icon="success"> lorem ipsum dolor sit amet </v-alert>
+    <v-alert icon="success" color="primary">
+      lorem ipsum dolor sit amet
+    </v-alert>
+    <v-alert icon="success" color="secondary">
+      lorem ipsum dolor sit amet
+    </v-alert>
+    <v-alert icon="success" color="info"> lorem ipsum dolor sit amet </v-alert>
+    <v-alert icon="success" color="warning">
+      lorem ipsum dolor sit amet
+    </v-alert>
+    <v-alert icon="success" color="error"> lorem ipsum dolor sit amet </v-alert>
+  </div>
+
+  <h2 class="font-bold text-lg mt-8 mb-2">Dismissable</h2>
+  <div class="space-y-2">
+    <v-alert dismissable> lorem ipsum dolor sit amet </v-alert>
+    <v-alert dismissable color="primary"> lorem ipsum dolor sit amet </v-alert>
+    <v-alert dismissable color="secondary">
+      lorem ipsum dolor sit amet
+    </v-alert>
+    <v-alert dismissable color="info"> lorem ipsum dolor sit amet </v-alert>
+    <v-alert dismissable color="warning"> lorem ipsum dolor sit amet </v-alert>
+    <v-alert dismissable color="error"> lorem ipsum dolor sit amet </v-alert>
+  </div>
 </template>
