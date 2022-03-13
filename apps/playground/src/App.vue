9f5e7a83f29dfea07725d7d2a7c9d4e7da9a65dc
Warsono <warsono16694@gmail.com>
2022-03-13 15:37:10 +0700

    playground: improved base layout

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -20,26 +20,24 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <v-app-bar hide-toggle>
-    <div class="container mx-auto flex gap-3 items-center">
-      <v-logo img-class="h-8" />
-
-      <span class="font-semibold text-primary-500">UI Component</span>
-
-      <div class="flex-grow flex justify-end sm:hidden">
-        <v-btn text @click="isOpen = !isOpen">
-          <XIcon v-if="isOpen" class="w-6 h-6" />
-          <MenuIcon v-else class="w-6 h-6" />
-        </v-btn>
-      </div>
+  <div class="flex h-16 p-2 sm:px-4 items-center gap-2 border-b">
+    <div class="flex sm:hidden">
+      <v-btn icon text class="p-2" @click="isOpen = !isOpen">
+        <XIcon v-if="isOpen" class="w-6 h-6" />
+        <MenuIcon v-else class="w-6 h-6" />
+      </v-btn>
     </div>
-  </v-app-bar>
-
-  <div class="container mx-auto sm:mt-5 static">
-    <div class="flex flex-col sm:flex-row">
-      <div class="sm:w-3/12 z-20 w-8/12">
+    <div class="flex-grow justify-center sm:justify-start flex gap-2">
+      <v-logo img-class="h-6" />
+      <span class="font-semibold text-primary-500">UI Component</span>
+    </div>
+    <div class="flex w-10 sm:hidden"></div>
+  </div>
+  <main class="flex flex-col h-screen">
+    <div class="flex flex-col sm:flex-row flex-1 overflow-hidden">
+      <div class="w-full sm:w-3/12 sm:p-4 p-0">
         <div
-          class="z-20 fixed sm:relative sm:top-0 top-[80px] h-[calc(100vh-70px)] w-full bg-white px-4 sm:px-0"
+          class="z-10 sm:z-0 fixed sm:relative sm:top-0 top-[80px] h-[calc(100vh-70px)] w-full bg-white sm:bg-transparent px-4 sm:px-0"
           :class="isOpen ? 'block' : 'hidden sm:block'"
         >
           <template v-for="(menu, idx) in menus" :key="idx">
@@ -62,11 +60,16 @@ onUnmounted(() => {
           </template>
         </div>
       </div>
-      <div class="w-full sm:w-9/12 p-4">
-        <RouterView />
+      <div class="flex flex-1 flex-col">
+        <div class="flex-1 overflow-y-auto paragraph p-4">
+          <router-view />
+        </div>
       </div>
     </div>
-  </div>
+    <div class="flex justify-center p-2 text-sm text-gray-400">
+      Copyright &copy; 2021-Present &middot; GITS Indonesia
+    </div>
+  </main>
 </template>
 
 <style></style>
