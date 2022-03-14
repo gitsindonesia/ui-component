6216eda153979a3a4bf65ba4c835ec45ed464e9b
Warsono <warsono16694@gmail.com>
2022-03-14 13:46:07 +0700

    playground: DocsTable: add default headers and add search

--- apps/playground/src/views/components/DocsTable.vue
+++ apps/playground/src/views/components/DocsTable.vue
@@ -1,21 +1,60 @@
 <script setup lang="ts">
+import {ref} from 'vue';
 import Docs from './Docs.vue';
 
-defineProps<{
-  title: string;
-  headers: any[];
-  items: any[];
-}>();
+const props = withDefaults(
+  defineProps<{
+    title: string;
+    headers?: any[];
+    items: any[];
+  }>(),
+  {
+    headers: () => [
+      {
+        text: 'Name',
+        value: 'prop',
+        sortable: false,
+      },
+      {
+        text: 'Type',
+        value: 'type',
+        sortable: false,
+      },
+      {
+        text: 'Required',
+        value: 'required',
+        sortable: false,
+      },
+      {
+        text: 'Default',
+        value: 'default',
+        sortable: false,
+      },
+      {
+        text: 'Description',
+        value: 'description',
+        sortable: false,
+      },
+    ],
+  },
+);
+
+const search = ref('');
 </script>
 
 <template>
   <Docs :title="title">
+    <div class="m-2">
+      <v-input v-model="search" placeholder="Search..." />
+    </div>
+
     <v-data-table
       :headers="headers"
       :items="items"
+      v-model:search="search"
       hide-footer
       no-shadow
-      class="mb-1"
+      class="mb-1 border-t !rounded-none"
     >
       <template #item.prop="{item}">
         <code v-text="item.prop"></code>
