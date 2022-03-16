b2197b4a5da9234308c8886d201281a7032bffac
Warsono <warsono16694@gmail.com>
2022-03-16 16:25:02 +0700

    feat: new `dark` color

--- packages/utils/colors.ts
+++ packages/utils/colors.ts
@@ -6,44 +6,45 @@ export const themeColors = [
   'success',
   'warning',
   'error',
+  'dark',
 ];
 
 export const getBgColor = (color: string, defaultColor = '') => {
   switch (color) {
     case 'primary':
-      return 'bg-primary-500'
+      return 'bg-primary-500';
     case 'secondary':
-      return 'bg-secondary-500'
+      return 'bg-secondary-500';
     case 'info':
-      return 'bg-info-500'
+      return 'bg-info-500';
     case 'warning':
-      return 'bg-warning-500'
+      return 'bg-warning-500';
     case 'success':
-      return 'bg-success-500'
+      return 'bg-success-500';
     case 'error':
-      return 'bg-error-500'
+      return 'bg-error-500';
     case 'default':
     default:
-      return defaultColor || 'bg-gray-600'
+      return defaultColor || 'bg-gray-600';
   }
-}
+};
 
 export const getTextColor = (color: string, defaultColor = '') => {
   switch (color) {
     case 'primary':
-      return 'text-primary-500'
+      return 'text-primary-500';
     case 'secondary':
-      return 'text-secondary-500'
+      return 'text-secondary-500';
     case 'info':
-      return 'text-info-500'
+      return 'text-info-500';
     case 'warning':
-      return 'text-warning-500'
+      return 'text-warning-500';
     case 'success':
-      return 'text-success-500'
+      return 'text-success-500';
     case 'error':
-      return 'text-error-500'
+      return 'text-error-500';
     case 'default':
     default:
-      return defaultColor || 'text-gray-900'
+      return defaultColor || 'text-gray-900';
   }
-}
+};
