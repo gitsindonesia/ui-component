6e63093b8b327ef39fc01a413c6a6e180d405784
Warsono <warsono16694@gmail.com>
2022-03-16 15:46:03 +0700

    docs: v-input

--- apps/playground/src/views/components/Inputs.vue
+++ apps/playground/src/views/components/Inputs.vue
@@ -2,21 +2,64 @@
 import {ref} from 'vue';
 import Docs from './Docs.vue';
 import DocsTitle from './DocsTitle.vue';
+import DocsTable from './DocsTable.vue';
+import {RiSearchLine} from 'vue-remix-icons';
 
 const breadcrumbs = ref([
   {
-    to: '/cms',
-    title: 'Dashboard',
-  },
-  {
-    to: '/cms/components',
+    to: '/components',
     title: 'Components',
   },
   {
-    to: '/cms/components/inputs',
-    title: 'v-input',
+    to: '/components/inputs',
+    title: 'Input',
   },
 ]);
+
+const propItems = [
+  {
+    prop: 'modelValue',
+    type: 'string',
+    required: 'false',
+    default: '',
+    description: 'Model value',
+  },
+  {
+    prop: 'value',
+    type: 'string',
+    required: 'false',
+    default: '',
+    description: 'Vee-validate value',
+  },
+  {
+    prop: 'name',
+    type: 'string',
+    required: 'false',
+    default: '',
+    description: 'Name',
+  },
+  {
+    prop: 'error',
+    type: 'boolean',
+    required: 'false',
+    default: 'false',
+    description: 'Error',
+  },
+  {
+    prop: 'error-messages',
+    type: 'array',
+    required: 'false',
+    default: 'false',
+    description: 'Error messages',
+  },
+  {
+    prop: 'readonly',
+    type: 'boolean',
+    required: 'false',
+    default: 'false',
+    description: 'Readonly',
+  },
+];
 </script>
 
 <template>
@@ -27,6 +70,41 @@ const breadcrumbs = ref([
       <v-input placeholder="Search..." />
     </template>
   </Docs>
+
+  <Docs
+    title="Append & Prepend Slots"
+    :code="`<div class=&quot;space-y-2&quot;>
+  <v-input placeholder=&quot;Search...&quot;>
+    <template #prepend>
+      <RiSearchLine class=&quot;fill-current ml-3 w-6 h-6&quot; />
+    </template>
+  </v-input>
+
+  <v-input placeholder=&quot;Search...&quot;>
+    <template #append>
+      <v-btn class=&quot;m-1&quot; color=&quot;primary&quot;>Search</v-btn>
+    </template>
+  </v-input>
+</div>`"
+  >
+    <template #preview>
+      <div class="space-y-2">
+        <v-input placeholder="Search...">
+          <template #prepend>
+            <RiSearchLine class="fill-current ml-3 w-6 h-6" />
+          </template>
+        </v-input>
+
+        <v-input placeholder="Search...">
+          <template #append>
+            <v-btn class="m-1" color="primary">Search</v-btn>
+          </template>
+        </v-input>
+      </div>
+    </template>
+  </Docs>
+
+  <DocsTable title="Props" :items="propItems" />
 </template>
 
 <style scoped></style>
