03a34d696cae68a9c7f1194e8018ca944a31802e
Warsono <warsono16694@gmail.com>
2022-03-16 18:53:58 +0700

    docs: VFormGroup

---
+++ packages/form-group/src/VFormGroup.stories.ts
@@ -0,0 +1,25 @@
+import {Story} from '@storybook/vue3';
+import VFormGroup from './VFormGroup.vue';
+
+export default {
+  title: 'Components/Form/FormGroup',
+  component: VFormGroup,
+  argTypes: {},
+  args: {
+    label: 'Label',
+  },
+};
+
+const Template: Story<{}> = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: {VFormGroup},
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return {args};
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: `<VFormGroup v-bind='args'>Content</VFormGroup>`,
+});
+
+export const Default = Template.bind({});
+Default.args = {};
