43b53bdcbddbd63c510dee140ace6152fd2aa2fb
Warsono <warsono16694@gmail.com>
2022-03-24 10:51:40 +0700

    chore: setup unplugin-vue-components

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -4,10 +4,18 @@ import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import Pages from 'vite-plugin-pages';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), Pages()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Pages(),
+    Components({
+      /* options */
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
