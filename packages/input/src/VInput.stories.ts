60b56e05ad3267833b8ededb38701d1b71797094
Warsono <warsono16694@gmail.com>
2022-05-09 11:22:59 +0700

    docs: v-input stories

--- packages/input/src/VInput.stories.ts
+++ packages/input/src/VInput.stories.ts
@@ -57,7 +57,7 @@ export const Variants: Story<VInputProps> = (args) => ({
   },
   // And then the `args` are bound to your component with `v-bind="args"`
   template: `
-    <div class='space-y-2'>
+    <div class='space-y-2 border-none'>
       <template v-for='color in themeColors' :key='color'>
         <div class='mb-2'>
           <label :for='color'>{{ color }}</label>
@@ -70,37 +70,86 @@ export const Variants: Story<VInputProps> = (args) => ({
 
 export const Default = Template.bind({});
 Default.args = {};
+Default.parameters = {
+  docs: {
+    source: {
+      code: '<v-input />',
+    },
+  },
+};
 
 export const Email = Template.bind({});
 Email.args = {
   type: 'email',
   value: 'example@domain.com',
 };
+Email.parameters = {
+  docs: {
+    source: {
+      code: '<v-input type="email" />',
+    },
+  },
+};
 
 export const Readonly = Template.bind({});
 Readonly.args = {
   readonly: true,
+};
+Readonly.parameters = {
+  docs: {
+    source: {
+      code: '<v-input readonly />',
+    },
+  },
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   disabled: true,
 };
+Disabled.parameters = {
+  docs: {
+    source: {
+      code: '<v-input disabled />',
+    },
+  },
+};
 
 export const Shadow = Template.bind({});
 Shadow.args = {
   shadow: true,
+};
+Shadow.parameters = {
+  docs: {
+    source: {
+      code: '<v-input shadow />',
+    },
+  },
 };
 
 export const Text = Template.bind({});
 Text.args = {
   text: true,
 };
+Text.parameters = {
+  docs: {
+    source: {
+      code: '<v-input text />',
+    },
+  },
+};
 
 export const Error = Template.bind({});
 Error.args = {
   error: true,
   errorMessages: ['Field is required'],
+};
+Error.parameters = {
+  docs: {
+    source: {
+      code: `<v-input error :error-messages="['Field is required']" />`,
+    },
+  },
 };
 
 export const Slots: Story<VInputProps> = (args) => ({
@@ -109,7 +158,7 @@ export const Slots: Story<VInputProps> = (args) => ({
     return {args};
   },
   template: `
-<div class="space-y-2">
+<div class="space-y-2 border-none">
   <v-input placeholder="Search...">
     <template #prepend>
       <RiSearchLine class="fill-current ml-2 -mr-2 w-5 h-5" />
@@ -124,6 +173,25 @@ export const Slots: Story<VInputProps> = (args) => ({
 </div>
   `,
 });
+Slots.parameters = {
+  docs: {
+    source: {
+      code: `
+<v-input placeholder="Search...">
+  <template #prepend>
+    <RiSearchLine class="fill-current ml-2 -mr-2 w-5 h-5" />
+  </template>
+</v-input>
+
+<v-input placeholder="Search...">
+  <template #append>
+    <RiSearchLine class="fill-current mr-3 w-5 h-5" />
+  </template>
+</v-input>
+      `,
+    },
+  },
+};
 
 export const Sizes: Story<VInputProps> = (args) => ({
   // Components used in your story `template` are defined in the `components` object
@@ -134,8 +202,20 @@ export const Sizes: Story<VInputProps> = (args) => ({
   },
   // And then the `args` are bound to your component with `v-bind="args"`
   template: `
-<div class="flex gap-2 flex-wrap">
+<div class="flex gap-2 flex-wrap border-none">
   <VInput v-for="size in sizes" :key="size" v-bind='args' :size="size" :placeholder="size"/>
 </div>
 `,
 });
+Sizes.parameters = {
+  docs: {
+    source: {
+      code: `<v-input size="xs" />
+<v-input size="sm" />
+<v-input size="md" />
+<v-input size="lg" />
+<v-input size="xl" />
+      `,
+    },
+  },
+};
