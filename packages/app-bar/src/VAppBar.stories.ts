81ca99a8a96e8a8406c4282a5773441ad27ecdbd
Warsono <warsono16694@gmail.com>
2022-05-09 09:32:55 +0700

    docs: v-app-bar stories

--- packages/app-bar/src/VAppBar.stories.ts
+++ packages/app-bar/src/VAppBar.stories.ts
@@ -1,9 +1,9 @@
-import MyAppBar from './VAppBar.vue';
+import VAppBar from './VAppBar.vue';
 import {Story, Meta} from '@storybook/vue3';
 
 export default {
   title: 'Components/AppBar',
-  component: MyAppBar,
+  component: VAppBar,
   argTypes: {},
   args: {
     mini: false,
@@ -15,35 +15,71 @@ export default {
 
 const Template: Story<{}> = (args) => ({
   // Components used in your story `template` are defined in the `components` object
-  components: {MyAppBar},
+  components: {VAppBar},
   // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     return {args};
   },
   // And then the `args` are bound to your component with `v-bind="args"`
-  template: `<MyAppBar v-bind="args">Hello</MyAppBar>`,
+  template: `<VAppBar v-bind="args">Hello</VAppBar>`,
 });
 
 export const Default = Template.bind({});
 Default.args = {};
+Default.parameters = {
+  docs: {
+    source: {
+      code: `<v-app-bar/>`,
+    },
+  },
+};
 
 export const Fixed = Template.bind({});
 Fixed.args = {
   fixed: true,
+};
+Fixed.parameters = {
+  docs: {
+    source: {
+      code: `<v-app-bar fixed/>`,
+    },
+  },
 };
 
 export const Mini = Template.bind({});
 Mini.args = {
   mini: true,
 };
+Mini.parameters = {
+  docs: {
+    source: {
+      code: `<v-app-bar mini/>`,
+    },
+  },
+};
 
 export const Dark = Template.bind({});
 Dark.args = {
   dark: true,
+};
+Dark.parameters = {
+  docs: {
+    source: {
+      code: `<v-app-bar dark/>`,
+    },
+  },
 };
 
 export const Custom = Template.bind({});
 Custom.args = {
   dark: true,
   class: 'rounded-lg',
+};
+
+Custom.parameters = {
+  docs: {
+    source: {
+      code: `<v-app-bar dark class="rounded-lg"/>`,
+    },
+  },
 };
