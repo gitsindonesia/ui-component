eac478e33d16cc4c099865c6026d0c9a77389a0c
Warsono <warsono16694@gmail.com>
2022-05-10 17:39:02 +0700

    refactor: use new v-input

--- packages/pages/src/Activation.vue
+++ packages/pages/src/Activation.vue
@@ -1,23 +1,24 @@
 <script setup>
-import { ref, toRefs, watch } from "vue";
-import { UserIcon, LockClosedIcon, ArrowLeftIcon } from "@heroicons/vue/outline";
-import { VInput, VInputGroup } from "@gits-id/input";
-import VBtn from "@gits-id/button";
-import { Form, Field } from "vee-validate";
-import { string, object, ref as YupRef } from "yup";
+import {ref, toRefs, watch} from 'vue';
+import {UserIcon, LockClosedIcon, ArrowLeftIcon} from '@heroicons/vue/outline';
+import {VInput, VInputGroup} from '@gits-id/input';
+import Button from '@gits-id/button';
+import {Form, Field, useForm} from 'vee-validate';
+import {string, object, ref as YupRef} from 'yup';
+import Alert from '@gits-id/alert';
 
 const props = defineProps({
   title: {
     type: String,
-    default: "Activate Your Account",
+    default: 'Activate Your Account',
   },
   subtitle: {
     type: String,
-    default: "Create a secure password to activate your account",
+    default: 'Create a secure password to activate your account',
   },
   message: {
     type: String,
-    default: "",
+    default: '',
   },
   loading: {
     type: Boolean,
@@ -25,27 +26,27 @@ const props = defineProps({
   },
   submitText: {
     type: String,
-    default: "Ubah Kata Sandi",
+    default: 'Ubah Kata Sandi',
   },
   passwordText: {
     type: String,
-    default: "Kata Sandi Baru",
+    default: 'Kata Sandi Baru',
   },
   passwordHint: {
     type: String,
-    default: "",
+    default: '',
   },
   passwordConfirmationText: {
     type: String,
-    default: "Konfirmasi Kata Sandi Baru",
+    default: 'Konfirmasi Kata Sandi Baru',
   },
   currentPasswordText: {
     type: String,
-    default: "Kata Sandi Lama",
+    default: 'Kata Sandi Lama',
   },
   currentPasswordHint: {
     type: String,
-    default: "",
+    default: '',
   },
   withOldPassword: {
     type: Boolean,
@@ -53,7 +54,7 @@ const props = defineProps({
   },
   passwordFormatError: {
     type: String,
-    default: "Kata Sandi harus berisi 8 karakter dan minimal ada 1 angka",
+    default: 'Kata Sandi harus berisi 8 karakter dan minimal ada 1 angka',
   },
   backArrow: {
     type: Boolean,
@@ -61,17 +62,21 @@ const props = defineProps({
   },
   backText: {
     type: String,
-    default: "Kembali ke halaman Login",
+    default: 'Kembali ke halaman Login',
   },
   passwordMatchError: {
     type: String,
-    default: "Tidak sesuai dengan kata sandi baru",
+    default: 'Tidak sesuai dengan kata sandi baru',
+  },
+  passwordConfirmationHint: {
+    type: String,
+    default: '',
   },
 });
 
-const { message, title, subtitle } = toRefs(props);
+const {message, title, subtitle} = toRefs(props);
 
-const emit = defineEmits(["submit"]);
+const emit = defineEmits(['submit']);
 
 const isError = ref(false);
 
@@ -80,7 +85,7 @@ watch(
   () => {
     isError.value = !!props.message;
   },
-  { immediate: true }
+  {immediate: true},
 );
 
 const schema = object({
@@ -92,24 +97,28 @@ const schema = object({
     .min(8)
     .matches(
       /[^\w\d]*(([0-9]+.*[A-Za-z]+.*)|[A-Za-z]+.*([0-9]+.*))/i,
-      props.passwordFormatError
+      props.passwordFormatError,
     )
     .label(props.passwordText),
   passwordConfirmation: string()
     .required()
-    .oneOf([YupRef("password"), null], props.passwordMatchError)
+    .oneOf([YupRef('password'), null], props.passwordMatchError)
     .label(props.passwordConfirmationText),
 });
 
-const onSubmit = (values) => {
-  emit("submit", values);
-};
+const {handleSubmit, errors} = useForm({
+  validationSchema: schema,
+});
+
+const onSubmit = handleSubmit((values) => {
+  emit('submit', values);
+});
 </script>
 
 <template>
   <div class="w-auto md:w-[400px] px-4">
     <slot name="back">
-      <v-btn
+      <Button
         v-if="backArrow"
         color="primary"
         text
@@ -119,7 +128,7 @@ const onSubmit = (values) => {
         <ArrowLeftIcon class="w-6 h-6" />
 
         <span v-if="backText" class="text-gray-1">{{ backText }}</span>
-      </v-btn>
+      </Button>
     </slot>
     <slot name="logo" />
     <slot name="header">
@@ -131,107 +140,52 @@ const onSubmit = (values) => {
       </div>
     </slot>
 
-    <v-alert v-model="isError" color="error" class="mt-5" icon dismissable>
+    <Alert v-model="isError" color="error" class="mt-5" icon dismissable>
       {{ message }}
-    </v-alert>
+    </Alert>
 
-    <Form v-slot="{ handleSubmit, errors }" :validation-schema="schema">
-      <form @submit.prevent="handleSubmit(onSubmit)">
-        <div class="mt-8">
-          <template v-if="withOldPassword">
-            <label
-              for="currentPassword"
-              class="mb-2 block font-medium"
-              :class="errors.currentPassword ? 'text-error' : ''"
-            >
-              {{ currentPasswordText }}
-            </label>
+    <form @submit.prevent="onSubmit">
+      <div class="mt-8">
+        <VInput
+          v-if="withOldPassword"
+          id="currentPassword"
+          name="currentPassword"
+          type="password"
+          :label="currentPasswordText"
+          :placeholder="currentPasswordText"
+          :hint="currentPasswordHint"
+        />
 
-            <Field v-slot="{ field, errors: fieldErrors }" name="currentPassword">
-              <VInputGroup
-                id="currentPassword"
-                v-bind="field"
-                :placeholder="currentPasswordText"
-                type="password"
-                name="currentPassword"
-                :error="!!fieldErrors.length"
-                :error-messages="[fieldErrors.length]"
-                error-class="min-h-[20px]"
-                class="mb-4"
-              >
-                <template v-if="currentPasswordHint" #hint>
-                  <div class="text-sm mt-1">
-                    {{ currentPasswordHint }}
-                  </div>
-                </template>
-              </VInputGroup>
-            </Field>
-          </template>
+        <VInput
+          id="password"
+          name="password"
+          type="password"
+          :label="passwordText"
+          :placeholder="passwordText"
+          :hint="passwordHint"
+        />
 
-          <label
-            for="password"
-            class="mb-2 block font-medium"
-            :class="errors.password ? 'text-error' : ''"
-          >
-            {{ passwordText }}
-          </label>
+        <VInput
+          id="passwordConfirmation"
+          name="passwordConfirmation"
+          type="passwordConfirmation"
+          :label="passwordConfirmationText"
+          :placeholder="passwordConfirmationText"
+          :hint="passwordConfirmationHint"
+        />
 
-          <Field v-slot="{ field, errors: fieldErrors }" name="password">
-            <VInputGroup
-              id="password"
-              v-bind="field"
-              :placeholder="passwordText"
-              type="password"
-              name="password"
-              :error="!!fieldErrors.length"
-              :error-messages="fieldErrors"
-              error-class="min-h-[20px]"
-              class="mb-4"
-            >
-              <template v-if="!fieldErrors.length" #hint>
-                <div class="text-sm mt-1">
-                  {{ passwordHint }}
-                </div>
-              </template>
-            </VInputGroup>
-          </Field>
-
-          <label
-            for="password"
-            class="mb-2 block font-medium"
-            :class="errors.passwordConfirmation ? 'text-error' : ''"
-          >
-            {{ passwordConfirmationText }}
-          </label>
-
-          <Field v-slot="{ field, errors: fieldErrors }" name="passwordConfirmation">
-            <VInputGroup
-              id="passwordConfirmation"
-              v-bind="field"
-              :placeholder="passwordConfirmationText"
-              type="password"
-              name="passwordConfirmation"
-              :error="!!fieldErrors.length"
-              :error-messages="[fieldErrors.length]"
-              error-class="min-h-[20px]"
-              class="mb-2"
-            >
-            </VInputGroup>
-          </Field>
-
-          <VBtn
-            type="submit"
-            color="primary"
-            block
-            uppercase
-            :disabled="loading"
-            :loading="loading"
-          >
-            {{ submitText }}
-          </VBtn>
-        </div>
-      </form>
-    </Form>
+        <Button
+          type="submit"
+          color="primary"
+          block
+          uppercase
+          :disabled="loading"
+          :loading="loading"
+        >
+          {{ submitText }}
+        </Button>
+      </div>
+    </form>
   </div>
 </template>
 
