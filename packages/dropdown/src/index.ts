20e71e083308e9064f3cc440670a0ece17a09ffa
Warsono <warsono16694@gmail.com>
2022-05-11 18:00:25 +0700

    chore(dropdown): fix build

---
+++ packages/dropdown/src/index.ts
@@ -0,0 +1,6 @@
+import Dropdown from './Dropdown.vue';
+import DropdownItem from './DropdownItem.vue';
+import DropdownButton from './DropdownButton.vue';
+
+export * from './types';
+export {Dropdown, DropdownButton, DropdownItem};
