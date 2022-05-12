4eecc1799dc33f1f2d5efcb977b0e17dd039e02f
Warsono <warsono16694@gmail.com>
2022-05-12 16:40:38 +0700

    test(v-btn): basic test for button

--- packages/button/src/VBtn.spec.ts
+++ packages/button/src/VBtn.spec.ts
@@ -1,12 +1,18 @@
 import {mount} from '@vue/test-utils';
 import VBtn from './VBtn.vue';
+import {test, expect} from 'vitest';
+
+const BUTTON_TEXT = 'My Button';
 
 test('mount component', async () => {
   expect(VBtn).toBeTruthy();
 
   const wrapper = mount(VBtn, {
     props: {},
+    slots: {
+      default: BUTTON_TEXT,
+    },
   });
 
-  // expect(wrapper.text()).toContain('Step 1');
+  expect(wrapper.text()).toContain(BUTTON_TEXT);
 });
