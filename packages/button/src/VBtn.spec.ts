d8514f89bd99c5bb5de6928dacff26052b41da4b
Warsono <warsono16694@gmail.com>
2022-05-12 21:43:25 +0700

    chore: fix vitest version

--- packages/button/src/VBtn.spec.ts
+++ packages/button/src/VBtn.spec.ts
@@ -1,18 +1,20 @@
 import {mount} from '@vue/test-utils';
 import VBtn from './VBtn.vue';
-import {test, expect} from 'vitest';
+import {test, expect, describe} from 'vitest';
 
 const BUTTON_TEXT = 'My Button';
 
-test('mount component', async () => {
-  expect(VBtn).toBeTruthy();
+describe('Button', () => {
+  test('mount component', async () => {
+    expect(VBtn).toBeTruthy();
 
-  const wrapper = mount(VBtn, {
-    props: {},
-    slots: {
-      default: BUTTON_TEXT,
-    },
+    const wrapper = mount(VBtn, {
+      props: {},
+      slots: {
+        default: BUTTON_TEXT,
+      },
+    });
+
+    expect(wrapper.text()).toContain(BUTTON_TEXT);
   });
-
-  expect(wrapper.text()).toContain(BUTTON_TEXT);
 });
