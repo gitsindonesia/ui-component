549b003852dfca25ce49dd34a0dc4c5ecc4666f3
Warsono <warsono16694@gmail.com>
2022-05-12 16:31:33 +0700

    fix(v-file-upload): fix Weak Cryptography

--- packages/file-input/src/VFileUpload.vue
+++ packages/file-input/src/VFileUpload.vue
@@ -290,28 +290,7 @@ const disabledClass = computed(() => {
   return disabled.value || readonly.value ? 'disabled-input' : '';
 });
 
-//generates random id;
-let guid = () => {
-  let s4 = () => {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  };
-  return (
-    s4() +
-    s4() +
-    '-' +
-    s4() +
-    '-' +
-    s4() +
-    '-' +
-    s4() +
-    '-' +
-    s4() +
-    s4() +
-    s4()
-  );
-};
+let guid = () => crypto.randomUUID()
 
 const dropzoneId = computed(() => guid());
 
