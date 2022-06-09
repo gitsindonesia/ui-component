45416e0557271fcfb953606c0cdd829605a67453
Warsono <warsono16694@gmail.com>
2022-06-09 14:46:30 +0700

    chore: upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,17 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../packages/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)", "../packages/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
   "framework": "@storybook/vue3",
   "core": {
-    "builder": "storybook-builder-vite"
+    "builder": "@storybook/builder-vite"
   },
-  async viteFinal(config, {configType}) {
-    config.resolve.dedupe = ["@storybook/client-api"]
-    // config.optimizeDeps.include = [...config.optimizeDeps.include, 'hoist-non-react-statics', 'fast-deep-equal', 'lodash']
-    return config
-},
-}
+
+  async viteFinal(config, {
+    configType
+  }) {
+    config.resolve.dedupe = ["@storybook/client-api"]; // config.optimizeDeps.include = [...config.optimizeDeps.include, 'hoist-non-react-statics', 'fast-deep-equal', 'lodash']
+
+    return config;
+  }
+
+};
