d5ba2bdd4f5af89aeee6ae025d19e37a71d71a69
Warsono <warsono16694@gmail.com>
2022-06-16 09:20:12 +0700

    chore: change menu item icon

--- packages/menu/src/VMenuItem.vue
+++ packages/menu/src/VMenuItem.vue
@@ -2,6 +2,7 @@
 import VMenuTooltip from './VMenuTooltip.vue';
 import {computed, PropType} from 'vue';
 import {Menu} from './types';
+import {Icon} from '@iconify/vue';
 
 const props = defineProps({
   item: Object as PropType<Menu>,
@@ -61,15 +62,7 @@ const centerClass = computed(() => {
     :class="[textColor, mini ? 'py-4' : '', centerClass]"
   >
     <span :class="mini ? '' : 'px-1 w-6'">
-      <svg
-        width="12"
-        height="12"
-        viewBox="0 0 12 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle cx="6" cy="6" r="5" stroke="currentColor" stroke-width="2" />
-      </svg>
+      <Icon icon="ic:outline-circle" class="w-4 h-4" />
     </span>
 
     <span :class="{'sm:hidden': mini && !expandHover, 'text-sm': small}">
