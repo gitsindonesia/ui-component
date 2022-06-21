30842d2b0da9d99cbd1a4e27c5f2d741cde5b727
Warsono <warsono16694@gmail.com>
2022-06-21 17:51:15 +0700

    chore(login): slots story

--- packages/pages/src/Login.stories.ts
+++ packages/pages/src/Login.stories.ts
@@ -25,9 +25,25 @@ export const Default = Template.bind({});
 Default.args = {};
 Default.storyName = 'Login';
 
-export const Custom = Template.bind({});
-Custom.args = {
+export const CustomButton = Template.bind({});
+CustomButton.args = {
   btnProps: {
     color: 'secondary',
   },
 };
+
+export const Slots: Story<{}> = (args) => ({
+  components: {Login, Auth},
+  setup() {
+    return {args};
+  },
+  template: `
+    <Auth>
+      <Login v-bind="args">
+        <template #logo>
+          <img class="bg-primary-500 p-2 mb-4 rounded-lg" src="https://mofin.id/wp-content/uploads/sites/6/2022/03/Logo-Mofin-Light-1.png" alt="Mofin" />
+        </template>
+      </Login>
+    </Auth>
+  `,
+});
