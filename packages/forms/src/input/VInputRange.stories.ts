05ee6f733e8229877aa8214cf09ae8b7ba30d979
Warsono <warsono16694@gmail.com>
2022-06-27 15:38:09 +0700

    feat(v-input-range): integrate vee-validate validation

--- packages/forms/src/input/VInputRange.stories.ts
+++ packages/forms/src/input/VInputRange.stories.ts
@@ -1,6 +1,9 @@
 import {Meta, Story} from '@storybook/vue3';
+import { useForm } from 'vee-validate';
 import {ref} from 'vue';
+import { object, string } from 'yup';
 import VInputRange from './VInputRange.vue';
+import VBtn from '@gits-id/button';
 
 export default {
   title: 'Forms/InputRange',
@@ -45,3 +48,43 @@ WithInput.parameters = {
     },
   },
 };
+
+
+export const Validation: Story<{}> = () => ({
+  components: {VInputRange, VBtn},
+  setup() {
+    const schema = object({
+      score: string()
+        .required()
+        .label('Range'),
+    });
+
+    const {handleSubmit, resetForm, values, errors} = useForm({
+      validationSchema: schema,
+      initialValues: {
+        score: 0,
+      },
+    });
+
+    const onSubmit = handleSubmit((values) => {
+      alert(JSON.stringify(values));
+    });
+
+    return {onSubmit, resetForm, values, errors};
+  },
+  template: `
+    <form @submit="onSubmit" class="border-none">
+      <v-input-range
+        wrapper-class="mb-4"
+        name="score"
+        label="Select Your Score"
+      />
+      <div class="mt-4">
+        <v-btn type="submit">Submit</v-btn>
+        <v-btn type="button" text @click="resetForm">Reset</v-btn>
+      </div>
+      <div class="my-5">Debug:</div>
+      <pre>{{ {errors, values} }}</pre>
+    </form>
+`,
+});
--- packages/forms/src/input/VInputRange.vue
+++ packages/forms/src/input/VInputRange.vue
@@ -2,6 +2,7 @@
 import {ref, toRefs, onMounted} from 'vue';
 import {useRange} from './useRange';
 import {useInputClasses} from '@gits-id/utils';
+import {useField} from 'vee-validate';
 
 const props = defineProps({
   modelValue: {
@@ -24,13 +25,35 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  name: {
+    type: String,
+    default: '',
+  },
+  rules: {
+    type: String,
+    default: '',
+  },
+  errorClass: {
+    type: String,
+    default: 'text-error-600 text-sm mt-1',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  labelClass: {
+    type: String,
+    default: 'block mb-1',
+  },
 });
 
-const {modelValue, showInput} = toRefs(props);
+const {modelValue, showInput, name, rules} = toRefs(props);
 
 const emit = defineEmits(['update:modelValue']);
 
-const value = ref(modelValue.value);
+const {value, errorMessage} = useField(name, rules, {
+  initialValue: modelValue,
+});
 
 const inputClass = useInputClasses();
 
@@ -54,144 +77,155 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex justify-center items-center">
-    <div class="relative w-full">
-      <div>
-        <div class="flex justify-between mb-3">
-          <label class="text-gray-700 text-sm" for="min">{{ minValue }} </label>
-          <label class="text-gray-700 text-sm" for="max">{{ maxValue }} </label>
-        </div>
-
-        <input
-          v-model="minValue"
-          type="range"
-          :step="step"
-          :min="min"
-          :max="max"
-          class="
-            absolute
-            pointer-events-none
-            appearance-none
-            z-20
-            h-2
-            w-full
-            opacity-0
-            cursor-pointer
-          "
-          @input="minTrigger"
-        />
-
-        <input
-          v-model="maxValue"
-          type="range"
-          :step="step"
-          :min="min"
-          :max="max"
-          class="
-            absolute
-            pointer-events-none
-            appearance-none
-            z-20
-            h-2
-            w-full
-            opacity-0
-            cursor-pointer
-          "
-          @input="maxTrigger"
-        />
-
-        <div class="relative z-10 h-2">
-          <div
-            class="
-              absolute
-              z-10
-              left-0
-              right-0
-              bottom-0
-              top-0
-              rounded-md
-              bg-gray-400
-              h-1
-            "
-          ></div>
-
-          <div
-            class="
-              absolute
-              z-20
-              top-0
-              bottom-0
-              rounded-md
-              bg-gradient-to-r
-              from-primary-400
-              to-primary-600
-              h-1
-            "
-            :style="'right:' + maxThumb + '%; left:' + minThumb + '%'"
-          ></div>
-
-          <div
-            class="
-              absolute
-              z-30
-              w-4
-              h-4
-              top-0
-              left-0
-              bg-primary-400
-              rounded-full
-              -mt-1.5
-            "
-            :style="'left: ' + minThumb + '%'"
-          ></div>
-
-          <div
-            class="
-              absolute
-              z-30
-              w-4
-              h-4
-              top-0
-              right-0
-              bg-primary-600
-              rounded-full
-              -mt-1.5
-            "
-            :style="'right: ' + maxThumb + '%'"
-          ></div>
-        </div>
-      </div>
-
-      <div
-        v-if="showInput"
-        class="
-          flex
-          items-center
-          justify-between
-          pt-5
-          space-x-4
-          text-sm text-gray-700
-        "
-      >
+  <div>
+    <label v-if="label" :class="labelClass">{{ label }}</label>
+    <div class="flex justify-center items-center">
+      <div class="relative w-full">
         <div>
+          <div class="flex justify-between mb-3">
+            <label class="text-gray-700 text-sm" for="min"
+              >{{ minValue }}
+            </label>
+            <label class="text-gray-700 text-sm" for="max"
+              >{{ maxValue }}
+            </label>
+          </div>
+
           <input
             v-model="minValue"
-            type="text"
-            maxlength="5"
-            :class="inputClass"
+            type="range"
+            :step="step"
+            :min="min"
+            :max="max"
+            class="
+              absolute
+              pointer-events-none
+              appearance-none
+              z-20
+              h-2
+              w-full
+              opacity-0
+              cursor-pointer
+            "
             @input="minTrigger"
           />
-        </div>
-        <div>
+
           <input
             v-model="maxValue"
-            type="text"
-            maxlength="5"
-            :class="inputClass"
+            type="range"
+            :step="step"
+            :min="min"
+            :max="max"
+            class="
+              absolute
+              pointer-events-none
+              appearance-none
+              z-20
+              h-2
+              w-full
+              opacity-0
+              cursor-pointer
+            "
             @input="maxTrigger"
           />
+
+          <div class="relative z-10 h-2">
+            <div
+              class="
+                absolute
+                z-10
+                left-0
+                right-0
+                bottom-0
+                top-0
+                rounded-md
+                bg-gray-400
+                h-1
+              "
+            ></div>
+
+            <div
+              class="
+                absolute
+                z-20
+                top-0
+                bottom-0
+                rounded-md
+                bg-gradient-to-r
+                from-primary-400
+                to-primary-600
+                h-1
+              "
+              :style="'right:' + maxThumb + '%; left:' + minThumb + '%'"
+            ></div>
+
+            <div
+              class="
+                absolute
+                z-30
+                w-4
+                h-4
+                top-0
+                left-0
+                bg-primary-400
+                rounded-full
+                -mt-1.5
+              "
+              :style="'left: ' + minThumb + '%'"
+            ></div>
+
+            <div
+              class="
+                absolute
+                z-30
+                w-4
+                h-4
+                top-0
+                right-0
+                bg-primary-600
+                rounded-full
+                -mt-1.5
+              "
+              :style="'right: ' + maxThumb + '%'"
+            ></div>
+          </div>
+        </div>
+
+        <div
+          v-if="showInput"
+          class="
+            flex
+            items-center
+            justify-between
+            pt-5
+            space-x-4
+            text-sm text-gray-700
+          "
+        >
+          <div>
+            <input
+              v-model="minValue"
+              type="text"
+              maxlength="5"
+              :class="inputClass"
+              @input="minTrigger"
+            />
+          </div>
+          <div>
+            <input
+              v-model="maxValue"
+              type="text"
+              maxlength="5"
+              :class="inputClass"
+              @input="maxTrigger"
+            />
+          </div>
         </div>
       </div>
+    </div>
+
+    <div v-if="errorMessage" :class="errorClass">
+      {{ errorMessage }}
     </div>
   </div>
 </template>
