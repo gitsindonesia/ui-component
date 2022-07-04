1e930207a22b791995f670b055773d86c74198b4
Warsono <warsono16694@gmail.com>
2022-07-04 14:31:51 +0700

    fix(list group): remove default gap

--- packages/list-group/src/ListGroup.vue
+++ packages/list-group/src/ListGroup.vue
@@ -11,7 +11,7 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-  <div class="flex flex-col gap-1 group overflow-hidden">
+  <div class="flex flex-col group overflow-hidden">
     <slot v-bind="props" />
   </div>
 </template>
