8cbc78ab2757cf86a02793c372dd0d7642745956
Warsono <warsono16694@gmail.com>
2022-07-12 16:02:35 +0700

    chore: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:vue/vue3-essential'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+};
