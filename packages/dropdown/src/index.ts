4ccf4b72d55495306935d7f7a9f5ac88bed351c0
Warsono <warsono16694@gmail.com>
2022-08-24 08:30:19 +0700

    fix(dropdown): improve package exports

--- packages/dropdown/src/index.ts
+++ packages/dropdown/src/index.ts
@@ -1,6 +1,5 @@
-import Dropdown from './Dropdown.vue';
-import DropdownItem from './DropdownItem.vue';
-import DropdownButton from './DropdownButton.vue';
-
+export {default} from './Dropdown.vue';
+export {default as Dropdown} from './Dropdown.vue';
+export {default as DropdownItem} from './DropdownItem.vue';
+export {default as DropdownButton} from './DropdownButton.vue';
 export * from './types';
-export {Dropdown, DropdownButton, DropdownItem};
