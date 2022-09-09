e0f242551effc8a3402b7b7a545ec0552acc44d8
Warsono <warsono16694@gmail.com>
2022-09-09 10:26:48 +0700

    chore(snippets): simple centered and social links only

--- packages/ui/src/snippets/Footer.stories.ts
+++ packages/ui/src/snippets/Footer.stories.ts
@@ -444,6 +444,7 @@ export const LanguageCurrencyNewsLetter: Story = (args) => ({
 </footer>
   `,
 });
+
 export const Newsletter: Story = (args) => ({
   components: {Logo, Icon, Button, VInput, Select},
   setup() {
@@ -550,6 +551,78 @@ export const Newsletter: Story = (args) => ({
           <Icon name="ri:twitter-fill" class="w-6 h-6" />
         </a>
       </div>
+    </div>
+  </div>
+</footer>
+  `,
+});
+
+export const SimpleCentered: Story = (args) => ({
+  components: {Icon},
+  setup() {
+    return {args};
+  },
+  template: `
+<footer class="dark:bg-gray-800 dark:text-gray-200 p-5 sm:px-0">
+  <div class="container mx-auto space-y-6">
+    <div class="flex justify-center gap-6">
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        About
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        Blog
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        Jobs
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        Press
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        Partners
+      </a>
+    </div>
+    <div class="flex gap-4 space-between justify-center">
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        <Icon name="ic:round-facebook" class="w-7 h-7" />
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        <Icon name="ri:instagram-line" class="w-7 h-7" />
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        <Icon name="ri:youtube-fill" class="w-7 h-7" />
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        <Icon name="ri:twitter-fill" class="w-7 h-7" />
+      </a>
+    </div>
+    <p class="text-center text-gray-400 dark:text-gray-400">&copy; 2022 GITS Indonesia. All rights reserved.</p>
+  </div>
+</footer>
+  `,
+});
+export const SocialLinksOnly: Story = (args) => ({
+  components: {Icon},
+  setup() {
+    return {args};
+  },
+  template: `
+<footer class="dark:bg-gray-800 dark:text-gray-200 p-5 sm:px-0">
+  <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 items-center">
+    <p class="text-left text-gray-400 dark:text-gray-400">&copy; 2022 GITS Indonesia. All rights reserved.</p>
+    <div class="flex gap-4 space-between justify-end">
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        <Icon name="ic:round-facebook" class="w-6 h-6" />
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        <Icon name="ri:instagram-line" class="w-6 h-6" />
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        <Icon name="ri:youtube-fill" class="w-6 h-6" />
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+        <Icon name="ri:twitter-fill" class="w-6 h-6" />
+      </a>
     </div>
   </div>
 </footer>
