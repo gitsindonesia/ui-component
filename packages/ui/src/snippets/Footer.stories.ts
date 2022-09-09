905c035c688a35a562da394123f79355c4b3b13c
Warsono <warsono16694@gmail.com>
2022-09-09 10:09:17 +0700

    chore(snippets): footer with newsletter

--- packages/ui/src/snippets/Footer.stories.ts
+++ packages/ui/src/snippets/Footer.stories.ts
@@ -1,6 +1,9 @@
 import {Meta, Story} from '@storybook/vue3';
 import Logo from '@gits-id/logo';
 import Icon from '@gits-id/icon';
+import {VInput} from '@gits-id/forms';
+import Button from '@gits-id/button';
+import Select from '@gits-id/select';
 
 export default {
   title: 'Snippets/Footer',
@@ -314,8 +317,129 @@ export const AutoDarkMode: Story = (args) => ({
         </div>
       </div>
     </div>
-    <hr class="mt-10 mb-5 dark:border-gray-500" />
+    <hr class="mt-10 mb-5 dark:border-gray-700" />
     <p class="text-center text-gray-500 dark:text-gray-500">&copy; 2022-Present GITS Indonesia. All rights reserved.</p>
+  </div>
+</footer>
+  `,
+});
+
+export const WithNewsletter: Story = (args) => ({
+  components: {Logo, Icon, Button, VInput, Select},
+  setup() {
+    return {args};
+  },
+  template: `
+<footer class="dark:bg-gray-800 dark:text-gray-200 p-5 sm:px-0">
+  <div class="container mx-auto">
+    <div class="flex gap-4 flex-col sm:flex-row">
+      <div class="sm:w-full">
+        <div class="grid grid-cols-1 sm:grid-cols-5 gap-5 w-full">
+          <div>
+            <h3 class="font-medium dark:text-white">Solutions</h3>
+            <ul class="space-y-3 mt-3">
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Marketing</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Analytics</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Commerce</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Insights</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="font-medium dark:text-white">Support</h3>
+            <ul class="space-y-3 mt-3">
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Pricing</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Documentation</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Guides</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">API Status</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="font-medium dark:text-white">Company</h3>
+            <ul class="space-y-3 mt-3">
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">About</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Blog</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Jobs</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Press</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="font-medium dark:text-white">Legal</h3>
+            <ul class="space-y-3 mt-3">
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Claim</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Privacy</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">Terms</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="font-medium dark:text-white">Language & Currency</h3>
+            
+            <Select hide-check-icon class="mt-3" placeholder="Language" :items="[{text: 'English', value: 'English'}]" />
+            <Select hide-check-icon class="mt-3" placeholder="Currency" :items="[{text: 'IDR', value: 'IDR'}]" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr class="mt-10 mb-5 dark:border-gray-700" />
+    <div class="grid grid-cols-1 sm:grid-cols-2 items-center">
+      <div class="space-y-2">
+        <h3 class="font-medium">Subscribe to our newletter</h3>
+        <p class="text-gray-500 dark:text-gray-400">The latest news, articles, and resources, sent to your inbox weekly.</p>
+      </div>
+      <div>
+        <form class="flex gap-3 justify-end">
+          <v-input placeholder="Enter your email" />
+          <Button color="primary">Subscribe</Button>
+        </form>
+      </div>
+    </div>
+    <hr class="mt-5 mb-5 dark:border-gray-700" />
+    <div class="grid grid-cols-1 sm:grid-cols-2">
+      <p class="text-gray-500 dark:text-gray-400">&copy; 2022-Present GITS Indonesia. All rights reserved.</p>
+      <div class="flex gap-4 space-between justify-end w-full">
+        <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+          <Icon name="ic:round-facebook" class="w-6 h-6" />
+        </a>
+        <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+          <Icon name="ri:instagram-line" class="w-6 h-6" />
+        </a>
+        <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+          <Icon name="ri:youtube-fill" class="w-6 h-6" />
+        </a>
+        <a href="#" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+          <Icon name="ri:twitter-fill" class="w-6 h-6" />
+        </a>
+      </div>
+    </div>
   </div>
 </footer>
   `,
