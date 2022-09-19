4c445d4f12053bfe6a41ea5ea74c3f24ef5e604e
Warsono <warsono16694@gmail.com>
2022-09-19 14:26:40 +0700

    feat(examples): add vue 3 example

---
+++ examples/vue/src/components/Alerts.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3 class="text-2xl font-semibold mb-2 mt-5">Alert</h3>
+  <div class="space-y-2">
+    <v-alert> Lorem ipsum solor sit amet </v-alert>
+    <v-alert color="primary"> Lorem ipsum solor sit amet </v-alert>
+    <v-alert color="secondary"> Lorem ipsum solor sit amet </v-alert>
+    <v-alert color="info"> Lorem ipsum solor sit amet </v-alert>
+    <v-alert color="warning"> Lorem ipsum solor sit amet </v-alert>
+    <v-alert color="success"> Lorem ipsum solor sit amet </v-alert>
+    <v-alert color="error"> Lorem ipsum solor sit amet </v-alert>
+  </div>
+</template>
---
+++ examples/vue/src/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Buttons from "../components/Buttons.vue";
+import Alerts from "../components/Alerts.vue";
+import Forms from "../components/Forms.vue";
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold text-gray-800">GITS UI Examples</h1>
+
+  <Alerts />
+
+  <Buttons />
+
+  <Forms />
+</template>
