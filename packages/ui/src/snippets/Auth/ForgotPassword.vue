bb4c3219c526f37afc12c5995f6c629d9becef71
Warsono <warsono16694@gmail.com>
2022-09-20 11:18:45 +0700

    chore(auth snippet): update layout auth

--- packages/ui/src/snippets/Auth/ForgotPassword.vue
+++ packages/ui/src/snippets/Auth/ForgotPassword.vue
@@ -4,6 +4,7 @@ import {ref} from 'vue';
 import VBtn from '@gits-id/button';
 import VLogo from '@gits-id/logo';
 import {VInput} from '@gits-id/forms';
+import Card from '@gits-id/card';
 
 const sent = ref(false);
 const showAlert = ref(false);
@@ -19,57 +20,59 @@ const onSubmit = handleSubmit((values) => {
 </script>
 
 <template>
-  <div>
-    <AuthCard>
-      <v-logo img-class="h-10 mx-auto" />
+  <div class="h-screen grid place-items-center">
+    <div class="sm:max-w-md w-full sm:mx-auto">
+      <Card>
+        <v-logo img-class="h-10 mx-auto" />
 
-      <template v-if="sent">
-        <h1 class="text-center text-3xl font-semibold mt-5 text-gray-800">
-          Email Sent
-        </h1>
-        <p class="text-gray-500 text-center mt-5">
-          It can take a few minutes to receive your password recovery link.
-        </p>
-        <p class="text-gray-500 text-center mt-3">
-          If you do not receive this link, please contact your administrator.
-        </p>
-      </template>
-      <template v-else>
-        <h1 class="text-center text-3xl font-semibold mt-4 text-gray-800">
-          Password Recovery
-        </h1>
-        <p class="text-gray-500 text-center mt-2">
-          We'll send reset password link to your email.
-        </p>
+        <template v-if="sent">
+          <h1 class="text-center text-3xl font-semibold mt-5 text-gray-800">
+            Email Sent
+          </h1>
+          <p class="text-gray-500 text-center mt-5">
+            It can take a few minutes to receive your password recovery link.
+          </p>
+          <p class="text-gray-500 text-center mt-3">
+            If you do not receive this link, please contact your administrator.
+          </p>
+        </template>
+        <template v-else>
+          <h1 class="text-center text-3xl font-semibold mt-4 text-gray-800">
+            Password Recovery
+          </h1>
+          <p class="text-gray-500 text-center mt-2">
+            We'll send reset password link to your email.
+          </p>
 
-        <v-alert v-model="showAlert" color="error" class="mt-6">
-          {{ error }}
-        </v-alert>
+          <v-alert v-model="showAlert" color="error" class="mt-6">
+            {{ error }}
+          </v-alert>
 
-        <form class="mt-4" @submit="onSubmit">
-          <v-input
-            label="Email"
-            placeholder="e.g. budi@gitsmail.com"
-            name="email"
-            prepend-icon="ri:mail-line"
-          />
-          <v-btn
-            :loading="isSubmitting"
-            type="submit"
-            class="mt-5"
-            color="primary"
-            block
-          >
-            Send Email
-          </v-btn>
-        </form>
-      </template>
-    </AuthCard>
-    <div class="text-center mt-2">
-      <v-btn color="primary" text to="/auth/login">
-        <template v-if="sent"> Sign in </template>
-        <template v-else> Ready to sign in? </template>
-      </v-btn>
+          <form class="mt-4" @submit="onSubmit">
+            <v-input
+              label="Email"
+              placeholder="e.g. budi@gitsmail.com"
+              name="email"
+              prepend-icon="ri:mail-line"
+            />
+            <v-btn
+              :loading="isSubmitting"
+              type="submit"
+              class="mt-5"
+              color="primary"
+              block
+            >
+              Send Email
+            </v-btn>
+          </form>
+        </template>
+      </Card>
+      <div class="text-center mt-2">
+        <v-btn color="primary" text to="/auth/login">
+          <template v-if="sent"> Sign in </template>
+          <template v-else> Ready to sign in? </template>
+        </v-btn>
+      </div>
     </div>
   </div>
 </template>
--- packages/ui/src/snippets/Auth/Login.vue
+++ packages/ui/src/snippets/Auth/Login.vue
@@ -20,57 +20,59 @@ const onSubmit = handleSubmit((values) => {
 </script>
 
 <template>
-  <div class="sm:max-w-md sm:mx-auto">
-    <v-card>
-      <v-logo img-class="h-10 mx-auto" />
+  <div class="h-screen grid place-items-center">
+    <div class="sm:max-w-md w-full sm:mx-auto">
+      <v-card>
+        <v-logo img-class="h-10 mx-auto" />
 
-      <h1 class="text-center text-3xl font-semibold mt-4 text-gray-800">
-        Welcome back!
-      </h1>
-      <p class="text-gray-500 text-center mt-2">Log in to your account</p>
+        <h1 class="text-center text-3xl font-semibold mt-4 text-gray-800">
+          Welcome back!
+        </h1>
+        <p class="text-gray-500 text-center mt-2">Log in to your account</p>
 
-      <v-alert v-model="showAlert" color="error" class="mt-6">
-        {{ error }}
-      </v-alert>
+        <v-alert v-model="showAlert" color="error" class="mt-6">
+          {{ error }}
+        </v-alert>
 
-      <form class="mt-4" @submit="onSubmit">
-        <v-input
-          label="Email"
-          placeholder="Email"
-          name="email"
-          prepend-icon="ri:mail-line"
-        />
-        <v-input
-          wrapper-class="mt-4"
-          label="Password"
-          placeholder="Password"
-          name="password"
-          :type="showPassword ? 'text' : 'password'"
-          prepend-icon="ri:lock-line"
-          :append-icon="showPassword ? 'ri:eye-off-line' : 'ri:eye-line'"
-          @click-append-icon="showPassword = !showPassword"
-        />
-        <v-checkbox
-          wrapper-class="mt-4"
-          name="rememberMe"
-          label="Remember me"
-        />
-        <v-btn
-          :loading="isSubmitting"
-          type="submit"
-          class="mt-4"
-          color="primary"
-          block
-        >
-          Login
+        <form class="mt-4" @submit="onSubmit">
+          <v-input
+            label="Email"
+            placeholder="Email"
+            name="email"
+            prepend-icon="ri:mail-line"
+          />
+          <v-input
+            wrapper-class="mt-4"
+            label="Password"
+            placeholder="Password"
+            name="password"
+            :type="showPassword ? 'text' : 'password'"
+            prepend-icon="ri:lock-line"
+            :append-icon="showPassword ? 'ri:eye-off-line' : 'ri:eye-line'"
+            @click-append-icon="showPassword = !showPassword"
+          />
+          <v-checkbox
+            wrapper-class="mt-4"
+            name="rememberMe"
+            label="Remember me"
+          />
+          <v-btn
+            :loading="isSubmitting"
+            type="submit"
+            class="mt-4"
+            color="primary"
+            block
+          >
+            Login
+          </v-btn>
+        </form>
+      </v-card>
+
+      <div class="text-center mt-2">
+        <v-btn color="primary" text to="/auth/forgot-password">
+          Forgot Password?
         </v-btn>
-      </form>
-    </v-card>
-
-    <div class="text-center mt-2">
-      <v-btn color="primary" text to="/auth/forgot-password">
-        Forgot Password?
-      </v-btn>
+      </div>
     </div>
   </div>
 </template>
