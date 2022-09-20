e2d3714d5a08e221cfc8cae5ac1da68eb3051bb2
Warsono <warsono16694@gmail.com>
2022-09-20 22:05:37 +0700

    chore: load logo css

--- packages/ui/src/snippets/Navbar/Navbar.vue
+++ packages/ui/src/snippets/Navbar/Navbar.vue
@@ -4,6 +4,7 @@ import Logo from '@gits-id/logo';
 import VBtn from '@gits-id/button';
 import Icon from '@gits-id/icon';
 import '@gits-id/avatar/dist/style.css';
+import '@gits-id/logo/dist/style.css';
 
 const menus = ref([
   {
