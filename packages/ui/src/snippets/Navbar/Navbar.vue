770c916d75e599e7f11f55cd528e32a33b9e782a
Warsono <warsono16694@gmail.com>
2022-09-20 13:45:03 +0700

    chore(snippet): update navbar: add mobile nav and update layout

--- packages/ui/src/snippets/Navbar/Navbar.vue
+++ packages/ui/src/snippets/Navbar/Navbar.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import {ref} from 'vue';
+import Logo from '@gits-id/logo';
+import VBtn from '@gits-id/button';
+import Icon from '@gits-id/icon';
 
 const menus = ref([
   {
     text: 'Home',
     to: '/',
+    active: true,
   },
   {
     text: 'About',
@@ -15,30 +19,63 @@ const menus = ref([
     to: '/contact',
   },
 ]);
+
+const isOpen = ref(false);
 </script>
 
 <template>
-  <header class="shadow">
+  <header class="shadow bg-white">
     <div
       class="
         container
         mx-auto
         px-4
         lg:px-0
-        flex
+        flex flex-col
+        sm:flex-row
         items-center
         justify-between
-        py-4
+        py-3
       "
     >
-      <a class="font-semibold" href="#"> Brand </a>
+      <div class="flex justify-between items-center w-full sm:w-auto gap-4">
+        <a class="font-semibold flex-1" href="#">
+          <Logo img-class="h-8" />
+        </a>
+        <v-btn class="sm:hidden" rounded icon @click="isOpen = !isOpen">
+          <Icon name="ri:menu-line" />
+        </v-btn>
+      </div>
 
-      <nav aria-label="Navigation">
-        <ul class="flex gap-4 items-center">
+      <!-- desktop navigation -->
+      <nav class="hidden sm:block" aria-label="Desktop Navigation">
+        <ul class="flex gap-6 items-center">
           <li v-for="menu in menus" :key="menu.text">
             <router-link
               exact-active-class="font-semibold text-primary"
               class="hover:text-primary"
+              :class="menu.active ? 'font-semibold text-primary' : ''"
+              :to="menu.to"
+            >
+              {{ menu.text }}
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+
+      <!-- mobile navigation -->
+      <nav
+        v-if="isOpen"
+        class="fixed sm:hidden top-[66px] bg-white shadow left-0 w-full"
+        aria-label="Mobile Navigation"
+      >
+        <ul class="flex flex-col py-1">
+          <li v-for="menu in menus" :key="menu.text">
+            <router-link
+              class="hover:bg-gray-100 hover:text-primary block px-4 py-3"
+              :class="
+                menu.active ? 'bg-gray-100 text-primary font-semibold' : ''
+              "
               :to="menu.to"
             >
               {{ menu.text }}
