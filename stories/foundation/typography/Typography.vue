3ecb74e9270418614161f2bb6bc592ee1b5c5275
Warsono <warsono16694@gmail.com>
2022-09-29 10:22:20 +0700

    feat(foundation typography): add size in px

--- stories/foundation/typography/Typography.vue
+++ stories/foundation/typography/Typography.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import defaultTheme from 'tailwindcss/defaultTheme';
+
+const remToPx = (rem: string) => +rem.replace('rem', '') * 16 + 'px';
 </script>
 
 <template>
@@ -7,11 +9,15 @@ import defaultTheme from 'tailwindcss/defaultTheme';
   <div class="space-y-5 divide-y">
     <div v-for="(size, name) in defaultTheme.fontSize" :key="name" class="pt-4">
       <div>
-        <div class="font-semibold text-lg">{{ `text-${name}` }}</div>
+        <div class="font-semibold text-xl">{{ `text-${name}` }}</div>
 
         <ul class="font-mono text-xs mt-1">
-          <li>Font size: {{ size[0] }}</li>
-          <li>Line height: {{ (size[1] as any).lineHeight }}</li>
+          <li>Font size: {{ size[0] }} ({{ remToPx(size[0]) }})</li>
+          <li>
+            Line height:
+            {{ (size[1] as any).lineHeight }}
+            ({{ remToPx((size[1] as any).lineHeight) }})
+          </li>
         </ul>
       </div>
       <p
