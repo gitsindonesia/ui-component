036dab5f268dcf6624277f0a8648ea6dd44bab04
Warsono <warsono16694@gmail.com>
2022-10-07 11:10:20 +0700

    docs: fix docs build

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,32 +1,10 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme';
 import './theme.css';
-import './tailwind.postcss';
-// import GitsUi from '@gits-id/ui';
-// import '@gits-id/ui/src/assets/transition.css';
-import DocsCanvas from './components/DocsCanvas.vue';
-import AlertsVModel from './components/alerts/VModel.vue';
-import AlertsDismissable from './components/alerts/Dismissable.vue';
-import AlertsSolid from './components/alerts/Solid.vue';
-import AlertsOutlined from './components/alerts/Outlined.vue';
-import AlertsBordered from './components/alerts/Bordered.vue';
-import AlertsTransition from './components/alerts/Transition.vue';
-import AlertsColors from './components/alerts/Colors.vue';
 
 export default {
   ...DefaultTheme,
   enhanceApp({app, router, siteData}) {
-    // gits ui
-    // app.use(GitsUi);
-    // docs canvas
-    app.component('DocsCanvas', DocsCanvas);
-    // alerts demo components
-    app.component('AlertsVModel', AlertsVModel);
-    app.component('AlertsDismissable', AlertsDismissable);
-    app.component('AlertsSolid', AlertsSolid);
-    app.component('AlertsOutlined', AlertsOutlined);
-    app.component('AlertsBordered', AlertsBordered);
-    app.component('AlertsTransition', AlertsTransition);
-    app.component('AlertsColors', AlertsColors);
+    //
   },
 };
