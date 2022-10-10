518b09fd94bdc3378459602f5ac5b7e025b196c0
Warsono <warsono16694@gmail.com>
2022-10-10 12:04:42 +0700

    feat(docs): add live preview component

---
+++ docs/.vitepress/theme/components/LivePreview.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {computed} from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    src: string;
+    width?: string | number;
+    height?: string | number;
+  }>(),
+  {
+    height: 400,
+  },
+);
+
+const source = computed(() => {
+  return `https://gits-ui.web.app/iframe.html?args=&id=${props.src}&viewMode=story`;
+});
+</script>
+
+<template>
+  <iframe
+    :src="source"
+    class="live-preview"
+    :width="width"
+    :height="height"
+  ></iframe>
+</template>
+
+<style>
+.live-preview {
+  width: 100%;
+  margin-top: 0.5em;
+  border: 1px solid #ddd;
+  border-radius: 0.5em;
+}
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,11 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme';
 import './theme.css';
+import LivePreview from './components/LivePreview.vue';
 
 export default {
   ...DefaultTheme,
   enhanceApp({app, router, siteData}) {
-    //
+    app.component('LivePreview', LivePreview);
   },
 };
