b0b2c399f15238e04cc69765797d1c7655863067
gretchelin <gretchelin_dev@outlook.co.id>
2022-11-10 17:13:01 +0700

    fix(Stepper.Item): fix divider dom position
    feat(Stepper.Divider): add linear and isActive prop
    fix(Stepper.Divider): fix styling issue

--- packages/steppers/src/StepperDivider.vue
+++ packages/steppers/src/StepperDivider.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 defineProps<{
   vertical?: boolean;
+  isActive?: boolean;
 }>();
 </script>
 
 <template>
   <div
     v-if="vertical"
-    class="absolute z-1 border-b border-gray-300 left-1/2 transform -translate-y-1/2 flex align-center items-center align-middle content-center stepper-divider-vertical"
+    class="absolute z-1  top-8 left-4 transform translate-y-[1px] stepper-divider-vertical w-[1px] h-full"
+    :class="isActive ? 'border-l-4 border-blue-300 -translate-x-[2px]' : 'border-l-2 border-gray-300 -translate-x-[1px]'"
   />
   <div
     v-else
-    class="absolute z-1 border-b border-gray-300 top-1/2 transform -translate-x-1/2 flex align-center items-center align-middle content-center stepper-divider h-2"
+    class="absolute z-1  top-4 left-0 transform -translate-x-1/2 flex align-center items-center align-middle content-center stepper-divider h-[1px]"
+    :class="isActive ? 'border-b-4 border-blue-300' : 'border-b-2 border-gray-300'"
   />
 </template>
 
