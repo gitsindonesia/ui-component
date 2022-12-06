a21baa46564464d8a22a7f14b5ae024f4cdbeec5
Warsono <warsono16694@gmail.com>
2022-12-06 17:56:08 +0700

    chore: improve bundle size

--- packages/list/vite.config.js
+++ packages/list/vite.config.js
@@ -4,9 +4,7 @@ import {resolve} from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   esbuild: {
     exclude: ['./src/**/**.stories.ts'],
   },
@@ -14,22 +12,28 @@ export default defineConfig({
     target: 'esnext',
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VListGroup',
+      name: 'VList',
       formats: ['es', 'cjs', 'iife', 'umd'],
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue', '@iconify/vue', 'vue-router', '@gits-id/collapsible'],
+      external: [
+        'vue',
+        '@iconify/vue',
+        'vue-router',
+        '@gits-id/collapsible',
+        '@gits-id/icon',
+      ],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue'
-        }
+          vue: 'Vue',
+        },
       },
       resolve: {
-        dedupe: "vue"
+        dedupe: 'vue',
       },
     },
   },
