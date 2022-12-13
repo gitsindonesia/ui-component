39abaa56ee0a79dfaf1676af00e301978253a132
gits-gretchelin <gretta@gits.id>
2022-12-13 19:05:18 +0700

    Fix issue with scroll position in VTabs (#64)

--- packages/tabs/src/VTabs.vue
+++ packages/tabs/src/VTabs.vue
@@ -146,7 +146,7 @@ const onTabClicked = ({index, event}: any) => {
     const width = el.offsetWidth;
     const parentWidth = parent.offsetWidth;
     const position = left + width / 2 - parentWidth / 2;
-    el.parentElement.scrollTo({
+    (tabContent.value as HTMLElement).scrollTo({
       left: position,
     });
   } else {
@@ -155,7 +155,7 @@ const onTabClicked = ({index, event}: any) => {
     } else if (nextEl && nextEl.offsetLeft > parent.offsetWidth) {
       moveNavigation(nextEl.offsetWidth);
     } else if (prevEl && prevEl.offsetLeft < el.offsetLeft) {
-      moveNavigation(-prevEl.offsetWidth);
+      moveNavigation(tabEl.offsetWidth);
     }
   }
 };
