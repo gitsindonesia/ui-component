643f10010f995ec19e7f3025f58a1ead0a9e34fa
Warsono <warsono16694@gmail.com>
2022-12-16 21:50:09 +0700

    docs: add GA

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,10 +2,17 @@
 import DefaultTheme from 'vitepress/theme';
 import './theme.css';
 import LivePreview from './components/LivePreview.vue';
+import VueGtag from "vue-gtag";
 
 export default {
   ...DefaultTheme,
-  enhanceApp({app}) {
+  enhanceApp({app, router}) {
     app.component('LivePreview', LivePreview);
+
+    // GA
+    app.use(VueGtag, {
+      pageTrackerScreenviewEnabled: true,
+      config: { id: "G-0SL9786TRN" }
+    })
   },
 };
