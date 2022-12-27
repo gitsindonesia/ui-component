a886c1d8171f6d2669d25f3105085b5cb99c58d9
Warsono <warsono16694@gmail.com>
2022-12-27 11:48:56 +0700

    feat(VMultiSelect): add sass

--- packages/multi-select/src/VMultiSelect.vue
+++ packages/multi-select/src/VMultiSelect.vue
@@ -188,7 +188,7 @@ const {
   loading,
   rules,
   disabled,
-  readonly
+  readonly,
 } = toRefs(props);
 
 // refs
@@ -302,8 +302,8 @@ const toggleSelectAll = () => {
 };
 
 const onInputClick = () => {
-  console.log({readonly, disabled})
-  if(!readonly.value && !disabled.value) {
+  console.log({readonly, disabled});
+  if (!readonly.value && !disabled.value) {
     isOpen.value = true;
   }
 };
@@ -612,182 +612,4 @@ watch(
   </div>
 </template>
 
-<style>
-:root {
-  --v-multi-select-border-color: var(--v-input-border-color);
-  --v-multi-select-border-radius: var(--v-input-border-radius);
-  --v-multi-select-bg-color: var(--v-input-bg-color);
-  --v-multi-select-text-color: theme('colors.gray.800');
-  --v-multi-select-height: auto;
-  --v-multi-select-placeholder-color: var(--v-input-placeholder-color);
-  --v-multi-select-border-radius: var(--v-input-border-radius);
-  --v-multi-select-padding-x: var(--v-input-padding-x);
-  --v-multi-select-padding-y: var(--v-input-padding-y);
-  --v-multi-select-font-size: var(--v-input-font-size);
-
-  /* label */
-  --v-multi-select-label-font-size: var(--v-input-label-font-size);
-  --v-multi-select-label-font-weight: var(--v-input-label-font-weight);
-  --v-multi-select-label-display: var(--v-input-label-display);
-  --v-multi-select-label-margin-bottom: var(--v-input-label-margin-bottom);
-
-  /* item */
-  --v-multi-select-item-font-size: theme('fontSize.base');
-  --v-multi-select-item-font-weight: theme('fontWeight.normal');
-  --v-multi-select-item-bg-color: theme('colors.white');
-  --v-multi-select-item-text-color: theme('colors.gray.800');
-  --v-multi-select-item-padding-x: theme('padding.3');
-  --v-multi-select-item-padding-y: theme('padding.2');
-  --v-multi-select-item-hover-bg-color: theme('colors.primary.500');
-  --v-multi-select-item-hover-text-color: theme('colors.white');
-
-  /* error */
-  --v-multi-select-error-border-color: theme('colors.error.500');
-
-  /* icon */
-  --v-multi-select-icon-color: theme('colors.gray.500');
-  --v-multi-select-check-icon-color: theme('colors.primary.500');
-
-  /* dropdown */
-  --v-multi-select-dropdown-bg-color: theme('colors.white');
-  --v-multi-select-dropdown-border-radius: theme('borderRadius.md');
-}
-
-.v-multi-select {
-  background: var(--v-multi-select-bg-color);
-  color: var(--v-multi-select-text-color);
-  border-radius: var(--v-multi-select-border-radius);
-  text-align: var(--v-multi-select-text-align);
-  border: 1px solid var(--v-multi-select-border-color);
-  height: var(--v-multi-select-height);
-  font-size: var(--v-multi-select-font-size);
-  font-weight: var(--v-multi-select-font-weight);
-  padding: var(--v-multi-select-padding-y) var(--v-multi-select-padding-x);
-
-  @apply relative;
-}
-
-.v-multi-select--error {
-  border-color: var(--v-multi-select-error-border-color);
-}
-
-.v-multi-select-error {
-  border: 1px solid var(--v-multi-select-error-border-color);
-
-  @apply focus-visible:ring-white focus-visible:ring-offset-error-300
-  focus-visible:border-error-500;
-}
-
-.v-multi-select-normal {
-  @apply border-gray-300 focus-visible:ring-white focus-visible:ring-offset-gray-300 focus-visible:border-primary-500;
-}
-
-.v-multi-select-label {
-  font-size: var(--v-multi-select-label-font-size);
-  font-weight: var(--v-multi-select-label-font-weight);
-  display: var(--v-multi-select-label-display);
-  margin-bottom: var(--v-multi-select-label-margin-bottom);
-}
-
-.v-multi-select-input {
-  @apply bg-transparent
-  w-full
-  cursor-default
-  focus:outline-none
-  gap-y-1
-  flex flex-wrap gap-2
-  items-center
-  focus-visible:ring-2
-  focus-visible:ring-opacity-75
-  focus-visible:ring-offset-2;
-}
-
-.v-multi-select-badges {
-  @apply flex flex-1 items-center gap-2 flex-wrap;
-}
-
-.v-multi-select-dropdown {
-  border-radius: var(--v-multi-select-dropdown-border-radius);
-  background: var(--v-multi-select-dropdown-bg-color);
-
-  @apply absolute inset-x-0
-  w-full
-  py-1
-  mt-3
-  overflow-auto
-  shadow-lg
-  max-h-60
-  ring-1 ring-black ring-opacity-5
-  focus:outline-none
-  sm:text-sm
-  px-0
-  z-10;
-}
-
-.v-multi-select-input-control::placeholder {
-  color: var(--v-select-placeholder-color);
-}
-
-.v-multi-select-input-control {
-  color: var(--v-multi-select-text-color);
-  min-width: 0;
-  max-width: 100%;
-
-  @apply border-none bg-transparent
-  p-1
-  focus:outline-none
-  focus:!ring-0
-  focus:!border-none
-  text-sm
-  flex-1;
-}
-
-.v-multi-select-action {
-  @apply flex items-center;
-}
-
-.v-multi-select-item:hover {
-  background: var(--v-multi-select-item-hover-bg-color);
-  color: var(--v-multi-select-item-hover-text-color);
-}
-
-.v-multi-select-item {
-  background: var(--v-multi-select-item-bg-color);
-  color: var(--v-multi-select-item-text-color);
-  font-size: var(--v-multi-select-item-font-size);
-  font-weight: var(--v-multi-select-item-font-weight);
-  padding: var(--v-multi-select-item-padding-y)
-    var(--v-multi-select-item-padding-x);
-
-  @apply cursor-default
-  select-none
-  relative flex gap-2 items-center;
-}
-
-.v-multi-select-item--focused {
-  @apply text-white bg-primary-500;
-}
-
-.v-multi-select-dropdown-loading {
-  @apply pl-6 pr-4 py-2 text-gray-500;
-}
-
-.v-multi-select-item-check {
-  @apply invisible flex items-center text-white
-  group-hover:text-white group-focus:text-white;
-}
-
-.v-multi-select-item--active .v-multi-select-item-check {
-  color: var(--v-multi-select-check-icon-color);
-
-  @apply visible;
-}
-
-.v-multi-select-item-text {
-  @apply block truncate;
-}
-
-.v-multi-select-icon {
-  color: var(--v-multi-select-icon-color);
-}
-</style>
+<style src="./VMultiSelect.scss" lang="scss"></style>
