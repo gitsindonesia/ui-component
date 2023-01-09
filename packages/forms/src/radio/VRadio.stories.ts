8dec22f554eca6b04e8ab43557f38722dab1e6d3
Gretta M.R <gretta@gits.id>
2023-01-09 11:09:40 +0700

    docs(VRadio): add story to showcase input state

--- packages/forms/src/radio/VRadio.stories.ts
+++ packages/forms/src/radio/VRadio.stories.ts
@@ -5,6 +5,7 @@ import {Meta, Story} from '@storybook/vue3';
 import {useForm} from 'vee-validate';
 import {object, string} from 'yup';
 import VBtn from '@gits-id/button';
+import {ref} from 'vue';
 
 export default {
   title: 'Forms/Radio',
@@ -105,3 +106,92 @@ export const Validation: Story<{}> = () => ({
     </form>
 `,
 });
+
+export const TestInputState: Story<{}> = (args) => ({
+  components: {VBtn, VRadio},
+  setup() {
+    const modelValue = ref();
+    const modelValue2 = ref();
+    const {handleSubmit, resetForm, values} = args.useForm ? useForm({
+      initialValues: {
+        radio: false,
+        radio2: false
+      }
+    }) : {
+      handleSubmit: (cb: any) => null,
+      resetForm: () => null,
+      values: {},
+    };
+
+    const onSubmit = handleSubmit((values: any) => {
+      alert(JSON.stringify(values));
+    });
+
+    const onChange = (val: any) => {
+      alert('onChange');
+    };
+
+    return {args, onSubmit, resetForm, values, modelValue, modelValue2, onChange};
+  },
+  template: `
+    <form @submit="onSubmit" class="border-none">
+    <h1 class="mb-8 font-semibold">{{ args.useForm ? 'with' : 'without' }} VeeValidate Form</h1>
+
+    <div class="flex flex-wrap gap-4">
+      <div class="flex-1">
+        <v-radio
+          name="radio"
+          label="Only Name"
+          value="name"
+        />
+        <div class="text-xs">
+          When used without vee validate, should not change "Vmodel" value or any other value unless
+          explicitly implemented<br/>
+          With vee validate, it should update form values under "radio" key
+        </div>
+      </div>
+
+      <div class="flex-1">
+        <v-radio
+          v-model="modelValue"
+          label="Only VModel"
+          value="vmodel"
+        />
+        <div class="text-xs">Should update "modelValue" only</div>
+      </div>
+
+      <div class="flex-1">
+        <v-radio
+          v-model="modelValue2"
+          name="radio2"
+          label="VModel and Name"
+          value="vmodelname"
+        />
+        <div class="text-xs">
+          Should update form values under "radio2" (w/ vee validate) key AND "modelValue2"
+        </div>
+      </div>
+      
+      <div class="flex-1">
+        <v-radio
+          label="Uncontrolled"
+          @change="onChange"
+          value="uncontrolled"
+        />
+        <div class="text-xs">Should not change any value unless explicitly implemented</div>
+      </div>
+      
+    </div>
+
+    <div class="mt-4">
+      <v-btn type="submit">Submit</v-btn>
+      <v-btn type="button" text @click="resetForm">Reset</v-btn>
+    </div>
+
+    <pre>{{ {values, modelValue, modelValue2} }}</pre>
+    </form>
+  `,
+});
+TestInputState.args = {
+  useForm: false,
+};
