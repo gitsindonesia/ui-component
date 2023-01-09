98bcad62687258e983abe3cdd9b77c61fd85594e
Gretta M.R <gretta@gits.id>
2023-01-09 10:25:39 +0700

    docs(VTextarea): add story to showcase input state

--- packages/forms/src/textarea/Textarea.stories.ts
+++ packages/forms/src/textarea/Textarea.stories.ts
@@ -187,3 +187,83 @@ export const ValidationMode: Story<{}> = () => ({
     </form>
   `,
 });
+
+export const TestInputState: Story<{}> = (args) => ({
+  components: {VBtn, VTextarea},
+  setup() {
+    const modelValue = ref('');
+    const modelValue2 = ref('');
+    const {handleSubmit, resetForm, values} = args.useForm ? useForm() : {
+      handleSubmit: (cb: any) => null,
+      resetForm: () => null,
+      values: {},
+    };
+
+    const onSubmit = handleSubmit((values: any) => {
+      alert(JSON.stringify(values));
+    });
+
+    const onChange = (val: any) => {
+      alert('onChange');
+    };
+
+    return {args, onSubmit, resetForm, values, modelValue, modelValue2, onChange};
+  },
+  template: `
+    <form @submit="onSubmit" class="border-none">
+    <h1 class="mb-8 font-semibold">{{ args.useForm ? 'with' : 'without' }} VeeValidate Form</h1>
+
+    <div class="flex flex-wrap gap-4">
+      <div class="flex-1">
+        <v-textarea
+          name="description"
+          label="Only Name"
+        />
+        <div class="text-xs">
+          When used without vee validate, should not change "Vmodel" value or any other value unless
+          explicitly implemented<br/>
+          With vee validate, it should update form values under "description" key
+        </div>
+      </div>
+
+      <div class="flex-1">
+        <v-textarea
+          v-model="modelValue"
+          label="Only VModel"
+        />
+        <div class="text-xs">Should update "modelValue" only</div>
+      </div>
+
+      <div class="flex-1">
+        <v-textarea
+          v-model="modelValue2"
+          name="description2"
+          label="VModel and Name"
+        />
+        <div class="text-xs">
+          Should update form values under "description2" (w/ vee validate) key AND "modelValue2"
+        </div>
+      </div>
+      
+      <div class="flex-1">
+        <v-textarea
+          label="Uncontrolled"
+          @change="onChange"
+        />
+        <div class="text-xs">Should not change any value unless explicitly implemented</div>
+      </div>
+      
+    </div>
+
+    <div class="mt-4">
+      <v-btn type="submit">Submit</v-btn>
+      <v-btn type="button" text @click="resetForm">Reset</v-btn>
+    </div>
+
+    <pre>{{ {values, modelValue, modelValue2} }}</pre>
+    </form>
+  `,
+});
+TestInputState.args = {
+  useForm: false,
+};
