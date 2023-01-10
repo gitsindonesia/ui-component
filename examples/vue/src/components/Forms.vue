188476b48821ec827067437b9a3b8002990ce24d
Warsono <warsono16694@gmail.com>
2023-01-10 15:46:41 +0700

    chore(starter): upgrade starter to v0.14.0 & simplify vue starter

--- examples/vue/src/components/Forms.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h3 class="text-2xl font-semibold mb-2 mt-5">Forms</h3>
-  <div class="space-y-2">
-    <v-input label="Name" placeholder="Full Name" />
-    <v-input label="Email" placeholder="Email" prepend-icon="ri:mail-line" />
-    <v-textarea label="Bio" placeholder="Bio" />
-    <v-checkbox id="toc" label="Agree to TOC?" />
-  </div>
-</template>
--- examples/vue/src/views/Home.vue
+++ examples/vue/src/views/Home.vue
@@ -1,18 +1,7 @@
 <script setup lang="ts">
-import Buttons from "../components/Buttons.vue";
-import Alerts from "../components/Alerts.vue";
-import Forms from "../components/Forms.vue";
-import AppBars from "../components/AppBars.vue";
+import Welcome from "@/components/Welcome.vue";
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold text-gray-800">GITS UI Examples</h1>
-
-  <AppBars />
-
-  <Alerts />
-
-  <Buttons />
-
-  <Forms />
+  <Welcome />
 </template>
