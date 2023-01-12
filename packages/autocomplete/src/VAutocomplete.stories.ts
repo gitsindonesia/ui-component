805a186c67a119e6369035b6159cd5dace75dd1c
Warsono <warsono16694@gmail.com>
2023-01-12 13:25:13 +0700

    feat: dark mode autocomplete

--- packages/autocomplete/src/VAutocomplete.stories.ts
+++ packages/autocomplete/src/VAutocomplete.stories.ts
@@ -4,6 +4,7 @@ import {object} from 'yup';
 import VAutocomplete from './VAutocomplete.vue';
 import VBtn from '@gits-id/button';
 import {Story} from '@storybook/vue3';
+import './VAutocomplete.dark.scss';
 
 const items = [
   {value: 1, text: 'Wade Cooper'},
@@ -240,3 +241,20 @@ TestInputState.args = {
   value: undefined,
   clearable: true,
 };
+
+export const DarkMode: Story = (args) => ({
+  components: {VAutocomplete},
+  setup() {
+    const selected = ref();
+    return {args, selected};
+  },
+  template: `
+<main class="dark dark:bg-neutral-900 p-6">
+  <VAutocomplete
+    v-bind="args"
+    v-model="selected"
+    label="Dark Mode"
+  />
+</main>
+  `,
+});
