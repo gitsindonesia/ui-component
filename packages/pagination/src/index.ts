50c14fc77eff92b6189601c15d29603742d3e0a7
Warsono <warsono16694@gmail.com>
2023-01-12 17:17:08 +0700

    fix: dark mode pagination and table pagination

--- packages/pagination/src/index.ts
+++ packages/pagination/src/index.ts
@@ -1,3 +1,5 @@
+import './VPagination.dark.scss';
+
 export {default} from './VPagination.vue';
 export {default as VPagination} from './VPagination.vue';
 export {default as VPaginationItem} from './VPaginationItem.vue';
