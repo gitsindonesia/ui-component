8dc709d41310d75cc8e34711cd195c5168291bd3
Warsono <warsono16694@gmail.com>
2023-01-13 18:20:32 +0700

    chore: remove unused import

--- packages/menus/src/VMenus.vue
+++ packages/menus/src/VMenus.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import {Menu, Dropdown, options} from 'floating-vue';
-import {List} from '@gits-id/list';
 import {computed} from 'vue';
 import Icon from '@gits-id/icon';
 import VMenusItem from './VMenusItem.vue';
-import 'floating-vue/dist/style.css';
 
 export type VMenuItem = InstanceType<typeof VMenusItem>['$props'];
 export type Placement = InstanceType<typeof Menu>['$props']['placement'];
