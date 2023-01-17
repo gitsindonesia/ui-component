77282208ba2da8d13061a9dee8e0fcd272cc4cf9
Warsono <warsono16694@gmail.com>
2023-01-17 15:34:05 +0700

    refactor: extract notification item into component

---
+++ starter/nuxt-admin/components/Admin/NotificationItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  notif: {
+    icon: string;
+    title: string;
+    message: string;
+  };
+}>();
+</script>
+
+<template>
+  <VListItem :prepend-icon="notif.icon">
+    <div>
+      <h3 class="font-medium">
+        {{ notif.title }}
+      </h3>
+      <p class="text-gray-600 text-sm truncate" :title="notif.message">
+        {{ notif.message }}
+      </p>
+    </div>
+  </VListItem>
+</template>
