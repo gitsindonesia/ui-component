bdf6a225e65281e1d412e24b5ab00cc33602d14e
Warsono <warsono16694@gmail.com>
2023-01-19 11:31:13 +0700

    chore(starter/nuxt-admin): add pinia

--- starter/nuxt-admin/nuxt.config.ts
+++ starter/nuxt-admin/nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@gits-id/ui-nuxt', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@gits-id/ui-nuxt',
+    '@nuxtjs/tailwindcss',
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: [
+          // automatically imports `defineStore`
+          'defineStore', // import { defineStore } from 'pinia'
+        ],
+      },
+    ],
+  ],
   typescript: {
     shim: false,
   },
