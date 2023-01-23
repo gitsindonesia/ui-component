315b756dd50d186a9a7cc0d462b4f45e1251d4af
Warsono <warsono16694@gmail.com>
2023-01-23 12:35:13 +0700

    feat(starter/nuxt-auth): update default pages design

--- starter/nuxt-auth/pages/protected.vue
+++ starter/nuxt-auth/pages/protected.vue
@@ -5,8 +5,19 @@ const {data, signOut} = useSession();
 </script>
 
 <template>
-  <div>I'm protected! Session data: {{ data }}</div>
-  <button @click="signOut()" class="rounded-xl shadow-xl p-2 m-2">
-    sign out
-  </button>
+  <VAppBar sticky shadow>
+    <div
+      class="container mx-auto flex justify-between items-center px-2 md:px-0"
+    >
+      <VLogo />
+
+      <VBtn @click="signOut()" text color="primary"> Sign out </VBtn>
+    </div>
+  </VAppBar>
+  <div class="container mx-auto p-6 md:px-0">
+    <h3 class="font-semibold text-xl text-gray-900">
+      Welcome back, {{ data?.user?.name }}!
+    </h3>
+    <p class="mt-2 text-gray-700">This is protected page!</p>
+  </div>
 </template>
