a15fb4e1d2f630405b09236c256d0bb6fa63c25d
Warsono <warsono16694@gmail.com>
2023-01-23 12:26:14 +0700

    feat(starter/nuxt-auth): get auth secret and auth origin from env

--- starter/nuxt-auth/server/api/auth/[...].ts
+++ starter/nuxt-auth/server/api/auth/[...].ts
@@ -3,6 +3,7 @@ import GithubProvider from 'next-auth/providers/github';
 import {NuxtAuthHandler} from '#auth';
 
 export default NuxtAuthHandler({
+  secret: process.env.AUTH_SECRET,
   pages: {
     signIn: '/auth/login',
   },
