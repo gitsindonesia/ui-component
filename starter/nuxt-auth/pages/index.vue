ee5f24ef66092dea746779657b7e507befc5c3e5
Warsono <warsono16694@gmail.com>
2023-01-23 12:35:13 +0700

    feat(starter/nuxt-auth): update default pages design

--- starter/nuxt-auth/pages/index.vue
+++ starter/nuxt-auth/pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container mx-auto p-6">
-    <h1 class="text-2xl font-semibold">Welcome to your gits app!</h1>
-    <p>
-      Checkout the page protected by `nuxt-auth` here:
-      <nuxt-link to="/protected" class="underline text-blue"
-        >Click me to test the auth setup!</nuxt-link
-      >
+    <h1 class="text-2xl font-semibold">
+      Welcome to <span class="font-semibol">nuxt-auth</span>!
+    </h1>
+    <p class="text-gray-700 mt-2">
+      Checkout the page protected here:
+      <VBtn to="/protected" size="sm" suffix-icon="ic:round-arrow-right-alt">
+        Click me to test the auth setup!
+      </VBtn>
     </p>
   </div>
 </template>
