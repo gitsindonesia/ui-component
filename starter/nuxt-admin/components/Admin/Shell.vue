e4d31eb3e46747db5ff17a6d63f597ec93def2be
Warsono <warsono16694@gmail.com>
2023-01-23 11:24:44 +0700

    refactor(starter/nuxt-admin): add `AdminShell`

---
+++ starter/nuxt-admin/components/Admin/Shell.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col">
+    <slot name="header" />
+    <div class="flex-1 flex">
+      <slot name="sidebar" />
+      <div class="p-6 flex flex-col flex-1">
+        <main class="flex-1">
+          <slot />
+        </main>
+        <slot name="footer" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+  @apply bg-gray-100 dark:bg-neutral-900 text-gray-800 dark:text-neutral-200;
+}
+</style>
