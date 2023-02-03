fd8a57ebcf04f4c8dcc6ba403feb9be9c7c2a677
Warsono <warsono16694@gmail.com>
2023-02-03 14:46:02 +0700

    feat(nuxt-admin): add default home page

---
+++ starter/nuxt-admin/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+useHead({
+  title: useAppConfig().admin.title,
+});
+</script>
+
+<template>
+  <div class="container mx-auto p-6 text-center">
+    Visit the
+    <VBtn
+      text
+      flush
+      color="primary"
+      to="/admin"
+      suffix-icon="ri:external-link-line"
+    >
+      Admin
+    </VBtn>
+    page to see the admin interface.
+  </div>
+</template>
