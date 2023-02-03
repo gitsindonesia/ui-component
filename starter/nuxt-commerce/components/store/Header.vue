c249c78d8baf1069d6ce06a205f99de3ef3e9314
Warsono <warsono16694@gmail.com>
2023-02-03 17:10:19 +0700

    feat(nuxt-commerce): add multi level categories menus

--- starter/nuxt-commerce/components/store/Header.vue
+++ starter/nuxt-commerce/components/store/Header.vue
@@ -4,12 +4,44 @@ const storeDrawer = useStoreDrawer();
 
 const menus = [
   {
-    title: 'Home',
-    link: '/',
+    text: 'Home',
+    to: '/',
   },
   {
-    title: 'Products',
-    link: '/products',
+    text: 'Products',
+    to: '/products',
+  },
+  {
+    text: 'Categories',
+    to: '/categories',
+    children: [
+      {
+        text: 'Category 1',
+        to: '/categories/category-1',
+        children: [
+          {
+            text: 'Sub Category 1',
+            to: '/categories/category-1/sub-category-1',
+          },
+          {
+            text: 'Sub Category 2',
+            to: '/categories/category-1/sub-category-2',
+          },
+          {
+            text: 'Sub Category 3',
+            to: '/categories/category-1/sub-category-3',
+          },
+        ],
+      },
+      {
+        text: 'Category 2',
+        to: '/categories/category-2',
+      },
+      {
+        text: 'Category 3',
+        to: '/categories/category-3',
+      },
+    ],
   },
 ];
 </script>
@@ -32,16 +64,46 @@ const menus = [
         </NuxtLink>
       </div>
 
-      <nav class="md:flex gap-4 items-center hidden">
-        <NuxtLink
-          exact-active-class="text-primary-500 font-semibold"
-          class="hover:text-primary-500"
-          v-for="menu in menus"
-          :key="menu.title"
-          :to="menu.link"
-        >
-          {{ menu.title }}
-        </NuxtLink>
+      <nav class="md:flex gap-6 items-center hidden">
+        <template v-for="menu in menus" :key="menu.text">
+          <VMenus v-if="menu.children" :label="menu.text">
+            <template #items>
+              <template v-for="child in menu.children" :key="child.text">
+                <VMenus
+                  :items="child.children"
+                  v-if="child.children"
+                  placement="right-start"
+                >
+                  <VMenusItem class="flex justify-between">
+                    {{ child.text }}
+                    <VIcon name="ri:arrow-right-s-fill" class="ml-auto" />
+                  </VMenusItem>
+                  <template #items>
+                    <template
+                      v-for="subChild in child.children"
+                      :key="subChild.text"
+                    >
+                      <VMenusItem :to="subChild.to">
+                        {{ subChild.text }}
+                      </VMenusItem>
+                    </template>
+                  </template>
+                </VMenus>
+                <VMenusItem v-else :to="child.to">
+                  {{ child.text }}
+                </VMenusItem>
+              </template>
+            </template>
+          </VMenus>
+          <NuxtLink
+            v-else
+            exact-active-class="text-primary-500 font-semibold"
+            class="hover:text-primary-500"
+            :to="menu.to"
+          >
+            {{ menu.text }}
+          </NuxtLink>
+        </template>
       </nav>
 
       <div>
@@ -57,8 +119,8 @@ const menus = [
     <VList>
       <VListItem class="!font-semibold">{{ appConfig.store.title }}</VListItem>
       <VListItemDivider />
-      <VListItem v-for="menu in menus" :key="menu.title" :to="menu.link">
-        {{ menu.title }}
+      <VListItem v-for="menu in menus" :key="menu.text" :to="menu.to">
+        {{ menu.text }}
       </VListItem>
     </VList>
   </VNavDrawer>
