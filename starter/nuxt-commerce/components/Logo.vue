025b8aede7fa68ae48c6534ea12543e01562af31
Warsono <warsono16694@gmail.com>
2023-02-20 14:45:30 +0700

    fix(nuxt-commerce): (logo) add link

--- starter/nuxt-commerce/components/Logo.vue
+++ starter/nuxt-commerce/components/Logo.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-const appConfig = useAppConfig()
+const appConfig = useAppConfig();
 </script>
 
 <template>
-  <nuxt-img
-    :src="appConfig.store.logo.url"
-    :alt="appConfig.store.logo.alt || 'Logo'"
-    class="h-8 w-auto sm:h-10"
-    width="89"
-    height="40"  
-  />
+  <nuxt-link to="/">
+    <nuxt-img
+      :src="appConfig.store.logo.url"
+      :alt="appConfig.store.logo.alt || 'Logo'"
+      class="h-8 w-auto sm:h-10"
+      width="89"
+      height="40"
+    />
+  </nuxt-link>
 </template>
