0a1ae4f2d6e1cea93c701affee4962606f86b976
Warsono <warsono16694@gmail.com>
2023-02-22 14:40:31 +0700

    feat(nuxt-admin): add vue3 apexcharts

--- starter/nuxt-admin/pages/admin/index.vue
+++ starter/nuxt-admin/pages/admin/index.vue
@@ -6,8 +6,40 @@ definePageMeta({
 useHead({
   title: 'Dashboard',
 });
+
+const options = ref({
+  chart: {
+    id: 'vuechart-example',
+  },
+  xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+  },
+});
+
+const series = ref([
+  {
+    name: 'series-1',
+    data: [30, 40, 45, 50, 49, 60, 70, 91],
+  },
+]);
+
+const {data: session} = useSession();
 </script>
 
 <template>
-  <AdminWelcome />
+  <div>
+    <AdminPageHeader
+      :title="`Welcome back, ${session?.user?.name}`"
+      subtitle="Track, manage and forecast your customers and orders."
+    />
+
+    <ClientOnly>
+      <apexchart
+        width="500"
+        type="bar"
+        :options="options"
+        :series="series"
+      ></apexchart>
+    </ClientOnly>
+  </div>
 </template>
