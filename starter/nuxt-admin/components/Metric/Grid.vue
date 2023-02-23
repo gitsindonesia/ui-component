89e0245745f7353732391ad085d60954b05e023b
Warsono <warsono16694@gmail.com>
2023-02-23 09:53:19 +0700

    refactor(nuxt-admin): rename `Metric` to `MetricItem`

--- starter/nuxt-admin/components/Metric/Grid.vue
+++ starter/nuxt-admin/components/Metric/Grid.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Metric from './Metric.vue';
+import Metric from './Item.vue';
 
 export type MetricItem = InstanceType<typeof Metric>['$props'];
 
@@ -12,6 +12,6 @@ defineProps<Props>();
 
 <template>
   <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-8">
-    <Metric v-for="item in items" :key="item.title" v-bind="item" />
+    <MetricItem v-for="item in items" :key="item.title" v-bind="item" />
   </div>
 </template>
