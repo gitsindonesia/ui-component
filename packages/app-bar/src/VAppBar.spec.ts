e70602cf383c394a0938de0d33528eae45db3a93
MuhamadAdrian <59602236+MuhamadAdrian@users.noreply.github.com>
2023-03-09 11:45:16 +0700

    test(VAppBar): add unit testing VAppBar component (#143)
    Co-authored-by: Muhamad Adrian Seftiana <adrian@gits.id>

--- packages/app-bar/src/VAppBar.spec.ts
+++ packages/app-bar/src/VAppBar.spec.ts
@@ -4,8 +4,22 @@ import {
   defaultShadows,
   defaultSizes,
 } from '@gits-id/theme/defaultTheme';
-import {describe, expect, test} from 'vitest';
+import {describe, expect, it, test} from 'vitest';
 import VAppBar from './VAppBar.vue';
+import {defineComponent, ref} from 'vue';
+import {createRouter, createWebHistory} from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: {
+        template: 'Hello',
+      },
+    },
+  ],
+});
 
 describe('VAppBar', () => {
   test('mount component', () => {
@@ -91,5 +105,113 @@ describe('VAppBar', () => {
       },
     });
     expect(wrapper).toBeTruthy();
+  });
+
+  describe('when navbar with toggle', () => {
+    test('will hide the app bar with v-model', async () => {
+      const WrapperComponent = defineComponent({
+        components: {
+          VAppBar,
+        },
+        setup() {
+          const isOpen = ref(true);
+          const toggle = () => {
+            isOpen.value = !isOpen.value;
+          };
+          return {
+            isOpen,
+            toggle,
+          };
+        },
+        template:
+          '<VAppBar v-model="isOpen">Toggle Test</VAppBar><button @click="toggle">Toggle</button>',
+      });
+
+      const wrapper = mount(WrapperComponent);
+
+      await wrapper.find('button').trigger('click');
+
+      expect(wrapper.html()).not.toContain('app-bar');
+    });
+
+    test('will hide the app bar with toggle slot', async () => {
+      const WrapperComponent = defineComponent({
+        components: {
+          VAppBar,
+        },
+        setup() {},
+        template:
+          '<VAppBar v-slot={toggle}>Toggle Test<button @click="toggle">Toggle</button></VAppBar>',
+      });
+
+      const wrapper = mount(WrapperComponent, {
+        global: {
+          plugins: [router],
+        },
+      });
+
+      await wrapper.find('button').trigger('click');
+
+      expect(wrapper.html()).not.toContain('app-bar');
+
+      router.push('/');
+    });
+  });
+
+  describe('when hideOnScroll true', () => {
+    document.body.innerHTML = `
+      <div>
+        <h1>Non Vue app</h1>
+        <div id="app"></div>
+      </div>
+    `;
+
+    it('hide app bar on scroll', async () => {
+      const WrapperComponent = defineComponent({
+        components: {
+          VAppBar,
+        },
+        setup() {},
+        template: `
+            <main class="mt-5 h-[200vh] bg-gray-50 text-white">
+              <VAppBar fixed hide-on-scroll transition="slide-down">
+                AppBar Hide On Scroll
+              </VAppBar>
+            </main>
+          `,
+      });
+
+      const wrapper = mount(WrapperComponent);
+
+      window.dispatchEvent(new Event('scroll'));
+      window.scrollTo(0, 500); // how to trigger the scroll ?
+
+      expect(wrapper.html()).toContain('app-bar'); // expected must not contain
+    });
+  });
+
+  describe('when elevateOnScroll true', () => {
+    it('add shadow to app bar', async () => {
+      const WrapperComponent = defineComponent({
+        components: {
+          VAppBar,
+        },
+        setup() {},
+        template: `
+            <main class="mt-5 h-[200vh] bg-gray-50 text-white">
+              <VAppBar elevate-on-scroll="lg" transition="slide-down">
+                AppBar Hide On Scroll
+              </VAppBar>
+            </main>
+          `,
+      });
+
+      const wrapper = mount(WrapperComponent);
+
+      window.dispatchEvent(new Event('scroll'));
+      window.scrollTo(0, 500); // how to trigger the scroll ?
+
+      expect(wrapper.html()).not.toContain('app-bar--shadow-lg'); // expected must contain
+    });
   });
 });
