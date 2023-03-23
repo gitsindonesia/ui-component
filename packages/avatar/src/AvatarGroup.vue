7723fba8f91b63dcbd09b6b3bf80e2eadc990517
Warsono <warsono16694@gmail.com>
2023-03-23 14:10:33 +0700

    fix(AvatarGroup): add spacing types

--- packages/avatar/src/AvatarGroup.vue
+++ packages/avatar/src/AvatarGroup.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import {PropType} from 'vue';
+
+export type AvatarGroupSpacing =
+  | number
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | '2xl';
+
 defineProps({
   spacing: {
-    type: [String, Number],
+    type: [String, Number] as PropType<AvatarGroupSpacing>,
     default: 'md',
   },
 });
