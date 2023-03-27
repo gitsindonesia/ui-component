a0474a3354114b89145ebd83de70ab7892cf20f5
Warsono <warsono16694@gmail.com>
2023-03-27 15:29:45 +0700

    chore(examples): upgrade deps

--- examples/nuxt-layer/nuxt.config.ts
+++ examples/nuxt-layer/nuxt.config.ts
@@ -1,16 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@gits-id/ui-nuxt', '@nuxtjs/tailwindcss'],
+  modules: ['@morpheme/nuxt', '@nuxtjs/tailwindcss'],
   typescript: {
     shim: false,
   },
-  extends: [
-    "@gits-id/nuxt-auth",
-    "@gits-id/nuxt-admin",
-  ],
+  extends: ['@morpheme/nuxt-auth', '@morpheme/nuxt-admin'],
   vite: {
     optimizeDeps: {
-      include: ['yup']
-    }
-  }
+      include: ['yup'],
+    },
+  },
 });
