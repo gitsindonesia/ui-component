55f5b6b90529b81ff7a62301ae05612f9dcae95c
Warsono <warsono16694@gmail.com>
2023-03-27 13:46:09 +0700

    fix(nuxt-admin): fix image alt

--- starter/nuxt-admin/components/Avatar.vue
+++ starter/nuxt-admin/components/Avatar.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 interface Props {
   src?: string | null;
+  alt?: string;
 }
 
-defineProps<Props>();
+withDefaults(defineProps<Props>(), {
+  alt: '',
+});
 </script>
 
 <template>
   <img
     v-if="src"
     :src="src"
+    :alt="alt"
     width="40"
     height="40"
     class="shrink-0 object-cover rounded-full w-10 h-10"
