731adea7042f3c586ddf81e7932fc544b5837a7f
Muhamad Adrian Seftiana <adrian@gits.id>
2023-03-28 10:36:11 +0700

    test(VSwitch): add more testing on VSwitch component

--- packages/switch/src/VSwitch.spec.ts
+++ packages/switch/src/VSwitch.spec.ts
@@ -1,13 +1,108 @@
-import {mount} from '@vue/test-utils';
+import {flushPromises, mount} from '@vue/test-utils';
 import VSwitch from './VSwitch.vue';
-import {test, expect} from 'vitest';
+import {test, expect, describe} from 'vitest';
+import {defineComponent, ref} from 'vue';
+import waitForExpect from 'wait-for-expect';
+import {boolean, object} from 'yup';
+import {useForm} from 'vee-validate';
 
-test('mount component', async () => {
-  expect(VSwitch).toBeTruthy();
+describe('VSwitch', () => {
+  test('mount component', async () => {
+    expect(VSwitch).toBeTruthy();
 
-  const wrapper = mount(VSwitch, {
-    props: {},
+    const wrapper = mount(VSwitch, {
+      props: {},
+    });
+
+    expect(wrapper.html()).toContain('div');
   });
 
-  expect(wrapper.html()).toContain('div');
+  test('can be off or on with modelValue', async () => {
+    const WrapperComponent = defineComponent({
+      components: {
+        VSwitch,
+      },
+      setup() {
+        const toggle = ref(true);
+        return {
+          toggle,
+        };
+      },
+      template: '<VSwitch v-model="toggle" />',
+    });
+
+    const wrapper = mount(WrapperComponent);
+
+    const buttonSwitch = wrapper.find('.v-switch-button');
+    expect(buttonSwitch.attributes('data-headlessui-state')).toEqual('checked');
+
+    await buttonSwitch.trigger('click');
+    expect(buttonSwitch.attributes('data-headlessui-state')).toEqual('');
+  });
+
+  test('render label', () => {
+    const wrapper = mount(VSwitch, {
+      props: {
+        label: 'Label',
+      },
+    });
+
+    expect(wrapper.text()).toContain('Label');
+  });
+
+  test('render hint', () => {
+    const wrapper = mount(VSwitch, {
+      props: {
+        hint: 'Hint',
+      },
+    });
+
+    expect(wrapper.text()).toContain('Hint');
+  });
+
+  test('can show error message when error appear', async () => {
+    const WrapperComponent = defineComponent({
+      components: {
+        VSwitch,
+      },
+      setup() {
+        const schema = object({
+          test: boolean()
+            .oneOf([true], 'You must agree to terms and condition')
+            .required()
+            .label('Test'),
+        });
+
+        const {handleSubmit} = useForm({
+          validationSchema: schema,
+        });
+
+        const onSubmit = handleSubmit((values) => {
+          // alert(JSON.stringify(values));
+        });
+
+        return {
+          onSubmit,
+        };
+      },
+      template: `<form @submit="onSubmit" class="border-none">
+        <VSwitch name="test" label="Label" />
+          <div class="mt-4">
+            <button type="submit">Submit</button>
+          </div>
+        </form>`,
+    });
+
+    const wrapper = mount(WrapperComponent);
+
+    await wrapper.find('form').trigger('submit');
+
+    await flushPromises();
+    await waitForExpect(() => {
+      expect(wrapper.html()).toContain('v-switch--error');
+      expect(wrapper.find('.v-switch--error').text()).toContain(
+        'You must agree to terms and condition',
+      );
+    });
+  });
 });
