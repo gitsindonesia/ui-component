71ce0d9462ab0af58069a855a3185ebd0946fd67
Warsono <warsono16694@gmail.com>
2023-03-31 14:19:07 +0700

    test: add BottomSheet test

--- packages/bottom-sheets/src/BottomSheet.test.ts
+++ packages/bottom-sheets/src/BottomSheet.test.ts
@@ -2,18 +2,60 @@ import {mount} from '@vue/test-utils';
 import BottomSheet from './BottomSheet.vue';
 
 describe('BottomSheet', () => {
-  test('renders properly', () => {
+  it('renders correctly', () => {
+    const wrapper = mount(BottomSheet);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('closes when the overlay is clicked', async () => {
     const wrapper = mount(BottomSheet, {
       props: {
         modelValue: true,
+        overlay: true,
       },
-      slots: {
-        default: 'Hello BottomSheet',
+      attachTo: document.body,
+      global: {
+        stubs: {
+          teleport: true,
+        },
       },
     });
+    await wrapper.vm.$nextTick();
+    const overlay = wrapper.find('.v-bottom-sheet-overlay');
+    await overlay.trigger('click');
+    expect(wrapper.emitted('update:modelValue')).toBeTruthy();
+    expect(wrapper.emitted('update:modelValue')[0]).toEqual([false]);
+  });
 
-    expect(wrapper.html()).toContain('<!--teleport start-->');
-    expect(wrapper.html()).toContain('<!--teleport end-->');
-    expect(wrapper.vm.modelValue).toBeTruthy();
+  it('sets the height of the bottom sheet', async () => {
+    const wrapper = mount(BottomSheet, {
+      attachTo: document.body,
+      global: {
+        stubs: {
+          teleport: true,
+        },
+      },
+    });
+    await wrapper.vm.$nextTick(); // wait for the component to render
+    const height = '500px';
+    await wrapper.vm.setHeight(height);
+    expect(wrapper.vm.getHeight()).toBe(0);
+    // expect(wrapper.find('.v-bottom-sheet').element.style.height).toBe(height);
+  });
+
+  it('opens and closes the bottom sheet', async () => {
+    const wrapper = mount(BottomSheet, {
+      attachTo: document.body,
+      global: {
+        stubs: {
+          teleport: true,
+        },
+      },
+    });
+    await wrapper.vm.$nextTick(); // wait for the component to render
+    await wrapper.vm.open();
+    expect(wrapper.vm.isOpen).toBe(true);
+    await wrapper.vm.close();
+    expect(wrapper.vm.isOpen).toBe(false);
   });
 });
