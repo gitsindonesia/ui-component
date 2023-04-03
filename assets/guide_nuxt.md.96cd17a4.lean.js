f8258f23ee2b29ca958f2daa99736cc94abcaf3f
gravitano <gravitano@users.noreply.github.com>
2023-04-03 16:23:26 +0000

    deploy: b792ca6b51127b2eb6109986beff55e0ea33766e

---
+++ assets/guide_nuxt.md.96cd17a4.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,N as l}from"./chunks/framework.ea512023.js";const f=JSON.parse('{"title":"Using Morpheme UI with Nuxt","description":"","frontmatter":{},"headers":[],"relativePath":"guide/nuxt.md"}'),t={name:"guide/nuxt.md"},o=l("",28),p=[o];function e(c,i,r,y,D,F){return n(),a("div",null,p)}const d=s(t,[["render",e]]);export{f as __pageData,d as default};
