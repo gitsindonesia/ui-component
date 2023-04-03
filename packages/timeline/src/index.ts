b747cfb92d1cfd926e4a4067d9a5c1f2ff526856
Warsono <warsono16694@gmail.com>
2023-04-03 14:03:10 +0700

    chore(VTimeline): export sub components

--- packages/timeline/src/index.ts
+++ packages/timeline/src/index.ts
@@ -1,2 +1,5 @@
-export {default as VTimeline} from './VTimeline.vue';
 export {default} from './VTimeline.vue';
+export {default as VTimeline} from './VTimeline.vue';
+export {default as VTimelineItem} from './VTimelineItem.vue';
+export {default as VTimelineItemContent} from './VTimelineItemContent.vue';
+export {default as VTimelineItemDot} from './VTimelineItemDot.vue';
