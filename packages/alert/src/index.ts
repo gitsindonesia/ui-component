80a206c9b0d02aff6d75eb0cbf893d212aa4bf9e
Warsono <warsono16694@gmail.com>
2023-04-05 13:32:19 +0700

    feat: export `VAlertGroup` component

--- packages/alert/src/index.ts
+++ packages/alert/src/index.ts
@@ -1,5 +1,3 @@
-import VAlert from './VAlert.vue';
-import './VAlert.dark.scss';
-
-export {VAlert};
-export default VAlert;
+export {default} from './VAlert.vue';
+export {default as VAlert} from './VAlert.vue';
+export {default as VAlertGroup} from './VAlertGroup.vue';
