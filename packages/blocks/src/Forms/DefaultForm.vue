aab39b4c38b4de3062321a410ae7e9415635ccd9
Warsono <warsono16694@gmail.com>
2023-05-12 14:10:54 +0700

    chore(blocks): add forms blocks

---
+++ packages/blocks/src/Forms/DefaultForm.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {VInput, VBtn} from '@morpheme/ui';
+</script>
+
+<template>
+  <form>
+    <VInput
+      prepend-icon="ic:round-person"
+      label="Name"
+      placeholder="Enter your name"
+      hint="Enter your full name"
+      wrapper-class="mb-4"
+    />
+    <VInput
+      prepend-icon="ic:round-email"
+      label="Email"
+      placeholder="Enter your email"
+      hint="Enter your email address"
+      wrapper-class="mb-4"
+    />
+    <VBtn color="primary">Login</VBtn>
+    <VBtn class="ml-3">Cancel</VBtn>
+  </form>
+</template>
---
+++ packages/blocks/src/Forms/InlineForm.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {VInput, VBtn} from '@morpheme/ui';
+</script>
+
+<template>
+  <form class="flex gap-2 items-end">
+    <VInput
+      prepend-icon="ic:round-person"
+      label="Name"
+      placeholder="Enter your name"
+    />
+    <VInput
+      prepend-icon="ic:round-email"
+      label="Email"
+      placeholder="Enter your email"
+    />
+    <VBtn color="primary">Login</VBtn>
+    <VBtn>Cancel</VBtn>
+  </form>
+</template>
