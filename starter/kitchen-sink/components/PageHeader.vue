355bb75260991e45093396bbdea494dd6000e34b
Warsono <warsono16694@gmail.com>
2023-05-31 13:49:30 +0700

    chore(kitchen sink): decrease font size

--- starter/kitchen-sink/components/PageHeader.vue
+++ starter/kitchen-sink/components/PageHeader.vue
@@ -7,7 +7,7 @@ defineProps<{
 
 <template>
   <div class="mb-6">
-    <h1 class="text-2xl font-semibold">{{ title }}</h1>
+    <h1 class="text-xl font-semibold">{{ title }}</h1>
     <p class="text-gray-500" v-if="subtitle">{{ subtitle }}</p>
   </div>
 </template>
