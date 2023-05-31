32c44ca19003669da7f26a9aac8c63fa60d50b9c
Warsono <warsono16694@gmail.com>
2023-05-31 09:46:01 +0700

    chore(nuxt commerce): upgrade Nuxt, Morpheme UI

--- starter/nuxt-commerce/nuxt.config.ts
+++ starter/nuxt-commerce/nuxt.config.ts
@@ -1,16 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@morpheme/nuxt', '@nuxtjs/tailwindcss', '@nuxt/image-edge'],
+
   typescript: {
     shim: false,
   },
+
   css: ['~/assets/css/globals.scss'],
-  extends: [
-    // '@morpheme/nuxt-auth'
-  ],
+
   // vite: {
   //   optimizeDeps: {
   //     include: ['yup'],
   //   },
   // }
+  extends: [
+    // '@morpheme/nuxt-auth'
+  ],
+
+  devtools: {
+    enabled: true
+  }
 });
