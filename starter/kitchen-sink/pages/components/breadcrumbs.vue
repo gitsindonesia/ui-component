420b1e04f47548bc169c62f48a8c1365232f7047
Warsono <warsono16694@gmail.com>
2023-05-31 16:08:18 +0700

    chore(kitchen sink): breadcrumbs

---
+++ starter/kitchen-sink/pages/components/breadcrumbs.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+const items = [
+  {
+    title: 'Home',
+    to: '/',
+  },
+  {
+    title: 'Account',
+    to: '/account',
+  },
+  {
+    title: 'Profile',
+    to: '/account/profile',
+  },
+  {
+    title: 'Settings',
+    to: '/account/profile/settings',
+    disabled: true,
+  },
+];
+</script>
+
+<template>
+  <PageHeader title="Breadcrumbs" />
+
+  <div class="space-y-4">
+    <VCard title="Default">
+      <div>
+        <VBreadcrumbs :items="items" />
+      </div>
+    </VCard>
+
+    <VCard title="Custom Divider">
+      <div>
+        <VBreadcrumbs :items="items" divider="-" />
+      </div>
+    </VCard>
+
+    <VCard title="Divider Slot">
+      <div>
+        <VBreadcrumbs :items="items">
+          <template #divider>
+            <VIcon
+              name="ri:arrow-right-s-line"
+              size="sm"
+              class="breadcrumbs-divider-icon"
+            />
+          </template>
+        </VBreadcrumbs>
+      </div>
+    </VCard>
+
+    <VCard title="Declarative">
+      <div>
+        <VBreadcrumbs>
+          <VBreadcrumbItem to="/"> Home </VBreadcrumbItem>
+          <VBreadcrumbsDivider />
+          <VBreadcrumbsItem to="/account"> Account </VBreadcrumbsItem>
+          <VBreadcrumbsDivider />
+          <VBreadcrumbsItem to="/account/profile" disabled>
+            Profile
+          </VBreadcrumbsItem>
+        </VBreadcrumbs>
+      </div>
+    </VCard>
+  </div>
+</template>
