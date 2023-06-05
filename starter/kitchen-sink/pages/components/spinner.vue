e0983a02162d4c08964546b0e3bf559d40889bec
Warsono <warsono16694@gmail.com>
2023-06-05 14:52:22 +0700

    chore(kitchen sink): spinner

---
+++ starter/kitchen-sink/pages/components/spinner.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <PageHeader title="Spinner" />
+
+  <div class="space-y-4">
+    <VCard title="Basic Example">
+      <div>
+        <VSpinner />
+      </div>
+    </VCard>
+
+    <VCard title="Colors">
+      <div>
+        <VSpinner />
+        <VSpinner color="primary" />
+        <VSpinner color="secondary" />
+        <VSpinner color="info" />
+        <VSpinner color="warning" />
+        <VSpinner color="success" />
+        <VSpinner color="error" />
+        <VSpinner color="dark" />
+      </div>
+    </VCard>
+
+    <VCard title="Sizes">
+      <div>
+        <v-spinner color="primary" />
+        <v-spinner color="primary" xSmall />
+        <v-spinner color="primary" small />
+        <v-spinner color="primary" large />
+        <v-spinner color="primary" xLarge />
+      </div>
+    </VCard>
+  </div>
+</template>
