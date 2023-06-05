df4bfc05c7909514ca0f31aa0d7b030f1ffbfaea
Warsono <warsono16694@gmail.com>
2023-06-05 14:45:09 +0700

    chore(kitchen sink): rating

---
+++ starter/kitchen-sink/pages/components/rating.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const rating = ref(3);
+</script>
+
+<template>
+  <PageHeader title="Rating" />
+
+  <div class="space-y-4">
+    <VCard title="Basic Example">
+      <div>
+        <VRating :rating="3" />
+      </div>
+    </VCard>
+
+    <VCard title="Basic Example">
+      <div>
+        <VRating v-model:rating="rating" />
+      </div>
+    </VCard>
+
+    <VCard title="Basic Example">
+      <div>
+        <VRating :rating="2.5" :increment="0.5" />
+      </div>
+    </VCard>
+  </div>
+</template>
