041254d7aadd8ffe6c9f96670ba6377265532ab7
Warsono <warsono16694@gmail.com>
2023-06-05 18:02:57 +0700

    chore(kitchen sink): fix (hack) v-menus position

--- starter/kitchen-sink/pages/components/menus.vue
+++ starter/kitchen-sink/pages/components/menus.vue
@@ -21,17 +21,17 @@ const items = [
   <div class="space-y-4">
     <VCard title="Basic Example">
       <div>
-        <VMenus :items="items" />
+        <VMenus class="inline-block" :items="items" />
       </div>
     </VCard>
     <VCard title="Right-aligned">
       <div>
-        <VMenus :items="items" right />
+        <VMenus class="inline-block" :items="items" right />
       </div>
     </VCard>
     <VCard title="Small">
       <div>
-        <VMenus :items="items" small />
+        <VMenus class="inline-block" :items="items" small />
       </div>
     </VCard>
     <VCard title="In App Bar">
@@ -39,16 +39,16 @@ const items = [
         <VAppBar shadow>
           <span class="font-semibold mr-4">Default</span>
           <nav aria-label="Nav" class="flex gap-3 items-center">
-            <VMenus label="Home" :items="items" />
-            <VMenus label="About" :items="items" />
-            <VMenus label="Profile" :items="items" />
+            <VMenus class="inline-block" label="Home" :items="items" />
+            <VMenus class="inline-block" label="About" :items="items" />
+            <VMenus class="inline-block" label="Profile" :items="items" />
           </nav>
         </VAppBar>
       </div>
     </VCard>
     <VCard title="Slots">
       <div>
-        <VMenus label="Account">
+        <VMenus class="inline-block" label="Account">
           <template #items>
             <VMenusItem prepend-icon="ri:user-line">Profile</VMenusItem>
             <VMenusItem prepend-icon="ri:settings-4-line">
