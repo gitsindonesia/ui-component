b6437ee6325624841f242542db13e17d578ab5ba
Warsono <warsono16694@gmail.com>
2023-06-05 18:00:32 +0700

    chore(kitchen sink): tooltip

--- starter/kitchen-sink/pages/components/tooltip.vue
+++ starter/kitchen-sink/pages/components/tooltip.vue
@@ -1,11 +1,55 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const placements = [
+  'top',
+  'top-start',
+  'top-end',
+  'bottom',
+  'bottom-start',
+  'bottom-end',
+  'right',
+  'right-start',
+  'right-end',
+  'left',
+  'left-start',
+  'left-end',
+];
+</script>
 
 <template>
-  <PageHeader title="Menu" />
+  <PageHeader title="Tooltip" />
 
   <div class="space-y-4">
     <VCard title="Basic Example">
-      <div></div>
+      <div class="flex gap-4">
+        <VTooltip class="inline-block">
+          <template #activator>
+            <VBtn> Button </VBtn>
+          </template>
+          <span>Hello :)</span>
+        </VTooltip>
+        <VTooltip class="mt-4 inline-block">
+          <template #activator>
+            <p>Hove on text</p>
+          </template>
+          <span>Hello :)</span>
+        </VTooltip>
+      </div>
+    </VCard>
+
+    <VCard title="Placement">
+      <div class="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        <VTooltip
+          v-for="placement in placements"
+          :key="placement"
+          :placement="placement"
+          class="inline-block"
+        >
+          <template #activator>
+            <VBtn block> {{ placement }} </VBtn>
+          </template>
+          <span>Hello :)</span>
+        </VTooltip>
+      </div>
     </VCard>
   </div>
 </template>
