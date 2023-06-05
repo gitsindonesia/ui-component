889711e8c16f41efa48288e3aea0735e93b87aee
Warsono <warsono16694@gmail.com>
2023-06-05 14:34:55 +0700

    chore(kitchen sink): pagination

---
+++ starter/kitchen-sink/pages/components/pagination.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+const page = ref(1);
+const totalItems = ref(100);
+const itemsPerPage = ref(10);
+</script>
+
+<template>
+  <PageHeader title="Pagination" />
+
+  <div class="space-y-4">
+    <VCard title="Basic Example">
+      <div>
+        <VPagination
+          v-model="page"
+          :total-items="totalItems"
+          :items-per-page="itemsPerPage"
+        />
+      </div>
+    </VCard>
+
+    <VCard title="Flat">
+      <div>
+        <VPagination
+          v-model="page"
+          :total-items="totalItems"
+          :items-per-page="itemsPerPage"
+          flat
+        />
+      </div>
+    </VCard>
+
+    <VCard title="Custom Style">
+      <div>
+        <VPagination
+          v-model="page"
+          :total-items="totalItems"
+          :items-per-page="itemsPerPage"
+          :style="{
+            '--v-pagination-item-active-bg-color': 'purple',
+            '--v-pagination-item-active-border-color': 'purple',
+          }"
+        />
+      </div>
+    </VCard>
+
+    <VCard title="Sizes">
+      <div>
+        <VPagination
+          v-model="page"
+          :total-items="totalItems"
+          :items-per-page="itemsPerPage"
+          size="sm"
+        />
+        <VPagination
+          v-model="page"
+          :total-items="totalItems"
+          :items-per-page="itemsPerPage"
+        />
+        <VPagination
+          v-model="page"
+          :total-items="totalItems"
+          :items-per-page="itemsPerPage"
+          size="lg"
+        />
+      </div>
+    </VCard>
+
+    <VCard title="Slots">
+      <div>
+        <VPagination
+          v-model="page"
+          :total-items="totalItems"
+          :items-per-page="itemsPerPage"
+        >
+          <template #btnPrev>Prev</template>
+          <template #btnNext>Next</template>
+          <template #btnFirst>First</template>
+          <template #btnLast>Last</template>
+        </VPagination>
+      </div>
+    </VCard>
+  </div>
+</template>
