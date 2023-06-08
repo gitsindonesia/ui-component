6d9d7b920f559e9271e9f225508c691c2d1a93c7
Warsono <warsono16694@gmail.com>
2023-06-08 13:49:08 +0700

    chore(Kitchen Sink): editor

--- starter/kitchen-sink/pages/forms/editor.vue
+++ starter/kitchen-sink/pages/forms/editor.vue
@@ -1,11 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const value = ref('');
+</script>
 
 <template>
   <PageHeader title="Editor" />
 
   <div class="space-y-4">
     <VCard title="Default">
-      <div>...</div>
+      <div>
+        <ClientOnly>
+          <VEditor v-model="value" />
+        </ClientOnly>
+      </div>
+    </VCard>
+
+    <VCard title="Label">
+      <div>
+        <ClientOnly>
+          <VEditor v-model="value" label="Label" />
+        </ClientOnly>
+      </div>
     </VCard>
   </div>
 </template>
