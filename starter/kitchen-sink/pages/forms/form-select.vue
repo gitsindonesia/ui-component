4bd66ec9db05c1a370ff3fb620f5498acdc9099a
Warsono <warsono16694@gmail.com>
2023-06-08 13:55:52 +0700

    chore(Kitchen Sink): form select

--- starter/kitchen-sink/pages/forms/form-select.vue
+++ starter/kitchen-sink/pages/forms/form-select.vue
@@ -1,11 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type {VFormSelectItem} from '@morpheme/forms';
+
+const items = ref<VFormSelectItem[]>([
+  {
+    text: 'Item 1',
+    value: '1',
+  },
+  {
+    text: 'Item 2',
+    value: '2',
+  },
+  {
+    text: 'Item 3',
+    value: '3',
+  },
+]);
+</script>
 
 <template>
   <PageHeader title="FormSelect" />
 
   <div class="space-y-4">
     <VCard title="Default">
-      <div>...</div>
+      <div>
+        <VFormSelect label="Label" name="option" :items="items" />
+      </div>
+    </VCard>
+
+    <VCard title="Disabled">
+      <div>
+        <VFormSelect label="Label" name="option" :items="items" disabled />
+      </div>
+    </VCard>
+
+    <VCard title="Error">
+      <div>
+        <VFormSelect label="Label" name="option" :items="items" error />
+      </div>
     </VCard>
   </div>
 </template>
