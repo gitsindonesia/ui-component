a3196b3456a4ba756f6f9a0cb025a8a4e007872d
Warsono <warsono16694@gmail.com>
2023-06-08 14:15:00 +0700

    chore(Kitchen Sink): radio

--- starter/kitchen-sink/pages/forms/radio.vue
+++ starter/kitchen-sink/pages/forms/radio.vue
@@ -1,11 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const selected = ref('1');
+</script>
 
 <template>
   <PageHeader title="Radio" />
 
   <div class="space-y-4">
     <VCard title="Default">
-      <div>...</div>
+      <div>
+        <VRadio label="Label" />
+      </div>
+    </VCard>
+
+    <VCard title="VModel">
+      <div>
+        <VRadio v-model="selected" value="1" label="A" />
+        <VRadio v-model="selected" value="2" label="B" />
+      </div>
+    </VCard>
+
+    <VCard title="Disabled">
+      <div>
+        <VRadio v-model="selected" value="1" label="A" />
+        <VRadio v-model="selected" value="2" label="B" />
+        <VRadio v-model="selected" value="3" label="C" disabled />
+      </div>
     </VCard>
   </div>
 </template>
