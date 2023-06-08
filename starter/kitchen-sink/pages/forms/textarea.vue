b7b89abbfa2ac55886c1286f4d5b5b5e3407998f
Warsono <warsono16694@gmail.com>
2023-06-08 14:19:14 +0700

    chore(Kitchen Sink): textarea

--- starter/kitchen-sink/pages/forms/textarea.vue
+++ starter/kitchen-sink/pages/forms/textarea.vue
@@ -5,7 +5,52 @@
 
   <div class="space-y-4">
     <VCard title="Default">
-      <div>...</div>
+      <div>
+        <VTextarea />
+      </div>
+    </VCard>
+
+    <VCard title="Label, Placeholder, and Hint">
+      <div>
+        <VTextarea label="Label" placeholder="Placeholder" hint="Hint" />
+      </div>
+    </VCard>
+
+    <VCard title="State">
+      <div>
+        <VTextarea
+          wrapper-class="mb-4"
+          label="Readonly"
+          placeholder="Placeholder"
+          hint="Hint"
+          readonly
+        />
+        <VTextarea
+          wrapper-class="mb-4"
+          label="Disabled"
+          placeholder="Placeholder"
+          hint="Hint"
+          disabled
+        />
+        <VTextarea
+          wrapper-class="mb-4"
+          label="Error"
+          placeholder="Placeholder"
+          hint="Hint"
+          error
+        />
+      </div>
+    </VCard>
+
+    <VCard title="With Counter">
+      <div>
+        <VTextarea
+          label="Readonly"
+          placeholder="Placeholder"
+          hint="Hint"
+          counter
+        />
+      </div>
     </VCard>
   </div>
 </template>
