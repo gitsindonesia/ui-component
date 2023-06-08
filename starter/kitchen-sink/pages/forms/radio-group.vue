46cce0e604667594d16deee022b37e1e3735efde
Warsono <warsono16694@gmail.com>
2023-06-08 14:16:59 +0700

    chore(Kitchen Sink): radio group

--- starter/kitchen-sink/pages/forms/radio-group.vue
+++ starter/kitchen-sink/pages/forms/radio-group.vue
@@ -1,11 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const items = ref([
+  {
+    text: 'Item 1',
+    value: 1,
+  },
+  {
+    text: 'Item 2',
+    value: 2,
+  },
+  {
+    text: 'Item 3',
+    value: 3,
+  },
+]);
+</script>
 
 <template>
   <PageHeader title="RadioGroup" />
 
   <div class="space-y-4">
     <VCard title="Default">
-      <div>...</div>
+      <div>
+        <VRadioGroup label="Label" :items="items" />
+      </div>
+    </VCard>
+
+    <VCard title="Inline">
+      <div>
+        <VRadioGroup label="Label" :items="items" inline />
+      </div>
+    </VCard>
+
+    <VCard title="Error">
+      <div>
+        <VRadioGroup label="Label" :items="items" error />
+      </div>
+    </VCard>
+
+    <VCard title="Disabled">
+      <div>
+        <VRadioGroup label="Label" :items="items" disabled />
+      </div>
+    </VCard>
+
+    <VCard title="Custom Class">
+      <div>
+        <VRadioGroup
+          :items="items"
+          label="Pick an option"
+          labelClass="!text-purple-500"
+          defaultClass="p-2 my-1 hover=bg-primary-50 border-grey-300 border rounded-lg cursor-pointer"
+          selectedClass="!border-primary-300 !text-primary-500"
+        />
+      </div>
     </VCard>
   </div>
 </template>
