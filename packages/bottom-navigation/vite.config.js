103072f4b1a144cd9386d5724ff3ff536830cf96
Warsono <warsono16694@gmail.com>
2023-06-12 16:53:58 +0700

    chore(BottomNavigation): externalize deps

--- packages/bottom-navigation/vite.config.js
+++ packages/bottom-navigation/vite.config.js
@@ -1,6 +1,6 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import {resolve} from 'path';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,7 +23,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue', '@morpheme/utils'],
+      external: ['vue', '@morpheme/utils', '@morpheme/theme', '@morpheme/icon', 'vue-router'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
