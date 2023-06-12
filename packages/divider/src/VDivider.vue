0d6c26993e0305ffaa32a9d69c58842c6f5aaefa
Warsono <warsono16694@gmail.com>
2023-06-12 12:21:43 +0700

    feat(Divider): add new `VDivider` component

---
+++ packages/divider/src/VDivider.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  inset?: boolean;
+  vertical?: boolean;
+}>();
+</script>
+
+<template>
+  <hr
+    role="separator"
+    :aria-orientation="vertical ? 'vertical' : 'horizontal'"
+    class="v-divider"
+    :class="{
+      'v-divider--inset': inset,
+      'v-divider--vertical': vertical,
+    }"
+  />
+</template>
