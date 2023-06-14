d4bcf3f9ddc8236afbc5e680459cfcafa48a4135
Warsono <warsono16694@gmail.com>
2023-06-14 16:26:26 +0700

    chore(Popover): export sub components

--- packages/popover/src/index.ts
+++ packages/popover/src/index.ts
@@ -1,3 +1,7 @@
 export {default} from './VPopover.vue';
 export {default as VPopover} from './VPopover.vue';
+export {default as VPopoverButton} from './VPopoverButton.vue';
+export {default as VPopoverGroup} from './VPopoverGroup.vue';
+export {default as VPopoverOverlay} from './VPopoverOverlay.vue';
+export {default as VPopoverPanel} from './VPopoverPanel.vue';
 export * from './api'
