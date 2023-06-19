8a28fd6863c11ea0f7d21e49b53bb7d58cecd380
Warsono <warsono16694@gmail.com>
2023-06-19 13:43:45 +0700

    feat(kitchen sink / inbox): update menus path

--- starter/kitchen-sink/components/inbox/Menus.vue
+++ starter/kitchen-sink/components/inbox/Menus.vue
@@ -18,16 +18,20 @@ const modelValue = defineModel<boolean>({
           <VBadge small color="primary">99+</VBadge>
         </template>
       </VListItem>
-      <VListItem to="/inbox/starred" prepend-icon="ri:star-line">Starred</VListItem>
-      <VListItem to="/inbox/sent" prepend-icon="ri:send-plane-line">Sent</VListItem>
-      <VListItem to="/inbox/drafts" prepend-icon="ri:file-copy-2-line">Drafts</VListItem>
-      <VListItem to="/inbox/trash" prepend-icon="ri:delete-bin-line">Trash</VListItem>
+      <VListItem to="/inbox?type=starred" prepend-icon="ri:star-line">Starred</VListItem>
+      <VListItem to="/inbox?type=sent" prepend-icon="ri:send-plane-line">Sent</VListItem>
+      <VListItem to="/inbox?type=drafts" prepend-icon="ri:file-copy-2-line"
+        >Drafts</VListItem
+      >
+      <VListItem to="/inbox?type=trash" prepend-icon="ri:delete-bin-line"
+        >Trash</VListItem
+      >
       <VListItemHeader to="/inbox" class="mt-8">Labels</VListItemHeader>
-      <VListItem to="/inbox/labels/personal" prepend-icon="ri:star-line"
+      <VListItem to="/inbox?label=personal" prepend-icon="ri:star-line"
         >Personal</VListItem
       >
-      <VListItem to="/inbox/labels/work" prepend-icon="ri:star-line">Work</VListItem>
-      <VListItem to="/inbox/labels/travel" prepend-icon="ri:star-line">Travel</VListItem>
+      <VListItem to="/inbox?label=work" prepend-icon="ri:star-line">Work</VListItem>
+      <VListItem to="/inbox?label=travel" prepend-icon="ri:star-line">Travel</VListItem>
     </VList>
   </VNavDrawer>
 </template>
