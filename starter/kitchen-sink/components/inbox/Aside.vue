942214e382475b337fc3b44dbc66b6b81ac57151
Warsono <warsono16694@gmail.com>
2023-06-19 14:22:47 +0700

    feat(kitchen sink / inbox): adjust aside menus

--- starter/kitchen-sink/components/inbox/Aside.vue
+++ starter/kitchen-sink/components/inbox/Aside.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts">
 const inboxDrawer = useInboxDrawer();
+
+const menus = [
+  {
+    text: "Mail",
+    icon: "ri:mail-line",
+    to: "/inbox",
+  },
+  {
+    text: "Chat",
+    icon: "ri:message-line",
+    to: "/inbox/chat",
+  },
+  {
+    text: "Spaces",
+    icon: "ri:user-line",
+    to: "/inbox/spaces",
+  },
+  {
+    text: "Meet",
+    icon: "ri:video-line",
+    to: "/inbox/meet",
+  },
+];
 </script>
 
 <template>
@@ -10,11 +33,28 @@ const inboxDrawer = useInboxDrawer();
     :class="{
       '!bg-gray-blue-50': $colorMode.preference === 'light',
     }"
+    :style="{
+      '--nav-drawer-mini-width': '72px',
+    }"
   >
     <VBtn prefix-icon="ri:menu-line" fab icon @click="inboxDrawer = !inboxDrawer" />
-    <VBtn prefix-icon="ri:mail-line" fab icon text class="mt-4" />
-    <VBtn prefix-icon="ri:message-line" fab icon text />
-    <VBtn prefix-icon="ri:user-line" fab icon text />
-    <VBtn prefix-icon="ri:video-line" fab icon text />
+
+    <ul class="flex gap-4 flex-col items-center mt-3">
+      <li
+        v-for="menu in menus"
+        :key="menu.text"
+        v-tooltip="{
+          content: menu.text,
+          placement: 'right',
+          theme: 'tooltip-black',
+          triggers: ['hover', 'focus'],
+          offset: [0, 8],
+        }"
+        class="w-full flex flex-col items-center cursor-pointer"
+      >
+        <VBtn fab icon text :to="menu.to" :prefix-icon="menu.icon" size="sm" rounded />
+        <p class="text-xs text-center">{{ menu.text }}</p>
+      </li>
+    </ul>
   </VNavDrawer>
 </template>
