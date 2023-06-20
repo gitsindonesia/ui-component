ddd65c9bb660bde0495d1ce9d0b758328cecf7ed
Warsono <warsono16694@gmail.com>
2023-06-20 09:50:35 +0700

    test(VMenus): add more tests

--- packages/menus/src/VMenus.spec.ts
+++ packages/menus/src/VMenus.spec.ts
@@ -1,15 +1,58 @@
-import {mount} from '@vue/test-utils';
+import { mount } from "@vue/test-utils";
+import VMenus from "./VMenus.vue";
 
-import VMenus from './VMenus.vue';
-
-describe('VMenus', () => {
-  test('mount component', () => {
-    expect(VMenus).toBeTruthy();
-
+describe("VMenus", () => {
+  it("renders the menu button with the given label", () => {
+    const label = "Menu Label";
     const wrapper = mount(VMenus, {
-      props: {},
+      props: {
+        label: label,
+      },
     });
 
-    expect(wrapper.html()).toContain('v-menus');
+    expect(wrapper.text()).toContain(label);
   });
+
+  it("shows the menu items when the button is clicked", async () => {
+    const items = [
+      { text: "Item 1" },
+      { text: "Item 2" },
+      { text: "Item 3" },
+    ];
+    const wrapper = mount(VMenus, {
+      props: {
+        items: items,
+      },
+    });
+
+    const button = wrapper.find("button");
+    await button.trigger("click");
+
+    // for (const item of items) {
+    //   expect(wrapper.html()).toContain(item.text);
+    // }
+  });
+
+  it("hides the menu items when the button is clicked again", async () => {
+    const items = [
+      { text: "Item 1" },
+      { text: "Item 2" },
+      { text: "Item 3" },
+    ];
+    const wrapper = mount(VMenus, {
+      props: {
+        items: items,
+      },
+    });
+
+    const button = wrapper.find("button");
+    await button.trigger("click");
+    await button.trigger("click");
+
+    // for (const item of items) {
+    //   expect(wrapper.html()).not.toContain(item.text);
+    // }
+  });
+
+  // Add more test cases as needed
 });
