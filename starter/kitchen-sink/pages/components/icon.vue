105779411c8c1717b48fd39db86107f66434fd1d
Warsono <warsono16694@gmail.com>
2023-06-21 18:10:55 +0700

    chore(kitchen sink): add morpheme icons example

--- starter/kitchen-sink/pages/components/icon.vue
+++ starter/kitchen-sink/pages/components/icon.vue
@@ -28,5 +28,57 @@
         <VIcon name="ri:home-2-line" class="text-error-500" />
       </div>
     </VCard>
+    <VCard title="Morpheme Icons">
+      <div>
+        <p class="font-medium mb-2">Default Collection</p>
+        <VIcon name="ri:home-line" />
+        <VIcon name="ic:round-local-activity" />
+        <VIcon name="ph:activity-bold" />
+
+        <p class="font-medium mb-2 mt-5">PascalCase</p>
+        <VIcon name="remix:HomeLine" />
+        <VIcon name="untitled:ActivityHeart" />
+        <VIcon name="iconsax:outline/Add" />
+        <VIcon name="iconsax:bulk/Add" />
+        <VIcon name="iconsax:broken/Add" />
+        <VIcon name="iconsax:linear/Add" />
+        <VIcon name="iconsax:solid/Add" />
+        <VIcon name="iconsax:twotone/Add" />
+
+        <p class="font-medium mb-2 mt-5">
+          PascalCase with suffix <code>Icon</code>
+        </p>
+        <VIcon name="remix:HomeLine" />
+        <VIcon name="untitled:ActivityHeart" />
+        <VIcon name="iconsax:outline/Add" />
+        <VIcon name="iconsax:bulk/Add" />
+        <VIcon name="iconsax:broken/Add" />
+        <VIcon name="iconsax:linear/Add" />
+        <VIcon name="iconsax:solid/Add" />
+        <VIcon name="iconsax:twotone/Add" />
+
+        <p class="font-medium mb-2 mt-5">HypenCase</p>
+        <VIcon name="remix:home-line" />
+        <VIcon name="untitled:activity-heart" />
+        <VIcon name="iconsax:outline/add" />
+        <VIcon name="iconsax:bulk/add" />
+        <VIcon name="iconsax:broken/add" />
+        <VIcon name="iconsax:linear/add" />
+        <VIcon name="iconsax:solid/add" />
+        <VIcon name="iconsax:twotone/add" />
+
+        <p class="font-medium mb-2 mt-5">
+          HypenCase with suffix <code>icon</code>
+        </p>
+        <VIcon name="remix:home-line-icon" />
+        <VIcon name="untitled:activity-heart-icon" />
+        <VIcon name="iconsax:outline/add-icon" />
+        <VIcon name="iconsax:bulk/add-icon" />
+        <VIcon name="iconsax:broken/add-icon" />
+        <VIcon name="iconsax:linear/add-icon" />
+        <VIcon name="iconsax:solid/add-icon" />
+        <VIcon name="iconsax:twotone/add-icon" />
+      </div>
+    </VCard>
   </div>
 </template>
