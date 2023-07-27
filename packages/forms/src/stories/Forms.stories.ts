d3a0914b97466df129ea69020fa079dd39d89b73
Warsono <warsono16694@gmail.com>
2023-07-27 10:35:24 +0700

    chore(Experiental/Forms): add forms validation playground

--- packages/forms/src/stories/Forms.stories.ts
+++ packages/forms/src/stories/Forms.stories.ts
@@ -1,5 +1,6 @@
 import {Meta, Story} from '@storybook/vue3';
 import Forms from './Forms.story.vue';
+import FormsWithValidation from './FormsWithValidation.story.vue';
 
 export default {
   title: 'Experimental/Forms/Playground',
@@ -12,4 +13,12 @@ export const Example: Story = (args) => ({
     return {args};
   },
   template: `<Forms v-bind='args'/>`,
+});
+
+export const WithValidation: Story = (args) => ({
+  components: {FormsWithValidation},
+  setup() {
+    return {args};
+  },
+  template: `<FormsWithValidation v-bind='args'/>`,
 });
