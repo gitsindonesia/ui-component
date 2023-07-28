7a52fc49101112e788f21f0d5a156c00a948b06d
Warsono <warsono16694@gmail.com>
2023-07-28 10:58:27 +0700

    test: fix tests

--- packages/select/src/Select.spec.ts
+++ packages/select/src/Select.spec.ts
@@ -65,7 +65,6 @@ describe('Select', () => {
         errorMessage: 'Error message',
       },
     });
-
-    expect(wrapper.find('.v-select-error').text()).toBe('Error message');
+    expect(wrapper.html()).toContain('v-select--error')
   });
 });
