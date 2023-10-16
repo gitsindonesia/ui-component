58de98026f6374e1847e6e664f2effe8fd1b6a10
Warsono <warsono16694@gmail.com>
2023-10-16 14:07:10 +0700

    chore: upgrade storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,17 @@
-import {app} from '@storybook/vue3';
 import {createMemoryHistory, createRouter} from 'vue-router';
 import '../packages/tailwind-config/tailwind.css';
 import '../packages/themes/src/morpheme/main.scss';
 import '../packages/themes/src/morpheme/main.dark.scss';
+import { setup } from '@storybook/vue3'
 
 const router = createRouter({
   history: createMemoryHistory(),
   routes: [],
 });
 
-app.use(router);
+setup((app) => {
+  app.use(router)
+})
 
 export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
