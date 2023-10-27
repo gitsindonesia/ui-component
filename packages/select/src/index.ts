e2c5dd0d96eddc85e3dc62a892656c0207ec6086
Warsono <warsono16694@gmail.com>
2023-10-27 10:05:53 +0700

    feat(Select): alias `Select` as `SelectMenu`

--- packages/select/src/index.ts
+++ packages/select/src/index.ts
@@ -1,9 +1,10 @@
 import './VSelect.dark.scss';
 export * from './types';
-export {default} from './VSelect.vue';
-export {default as VSelect} from './VSelect.vue';
-export {default as Select} from './Select.vue';
-export {default as SelectField} from './SelectField.vue';
-export {default as SelectSearchInput} from './SelectSearchInput.vue';
+export { default } from './VSelect.vue';
+export { default as VSelect } from './VSelect.vue';
+export { default as Select } from './Select.vue';
+export { default as SelectMenu } from './Select.vue';
+export { default as SelectField } from './SelectField.vue';
+export { default as SelectSearchInput } from './SelectSearchInput.vue';
 // alias
-export {default as SelectInput} from './SelectField.vue';
+export { default as SelectInput } from './SelectField.vue';
