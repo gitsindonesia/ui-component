06ee5d6ea7f164eab89194a11d34773ae0641164
Warsono <warsono16694@gmail.com>
2024-08-29 13:26:25 +0700

    chore: update @morpheme/nuxt dependency to 1.0.1-alpha.1+0486ec6e and nuxt to 3.13.0

--- starter/nuxt-minimal/nuxt.config.ts
+++ starter/nuxt-minimal/nuxt.config.ts
@@ -1,12 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@morpheme/nuxt', '@nuxtjs/tailwindcss'],
+
   typescript: {
     shim: false,
   },
+
   morpheme: {
     theme: 'morpheme',
     sass: true,
     darkMode: false,
   },
+
+  compatibilityDate: '2024-08-29',
 });
