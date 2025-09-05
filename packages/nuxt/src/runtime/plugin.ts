0c7e7a1d6f3dd788a6c50735dac9af04209eb35a
Warsono <warsono16694@gmail.com>
2025-09-05 14:38:30 +0700

    refactor: update SSR check in Nuxt plugin to use typeof window

--- packages/nuxt/src/runtime/plugin.ts
+++ packages/nuxt/src/runtime/plugin.ts
@@ -2,7 +2,7 @@ import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   // Add a dummy directive for SSR to prevent errors
-  if (import.meta.client) {
+  if (typeof window === 'undefined') {
     // SSR: Add dummy directive to prevent getSSRProps errors
     nuxtApp.vueApp.directive('tooltip', {
       getSSRProps: () => ({}),
